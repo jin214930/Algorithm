93529356cdea9e6aaa44e696616cbad61faeb6cc
Ward <91389299+jin214930@users.noreply.github.com>
2024-10-06 15:38:16 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int i;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> s >> i;
+  cout << s[i - 1];
+
+  return 0;
+}
