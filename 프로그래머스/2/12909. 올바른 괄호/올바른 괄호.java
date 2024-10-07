9b7d29c58f62aecbfeb5f767f6ae0eab6358c81e
Ward <91389299+jin214930@users.noreply.github.com>
2024-10-07 11:36:09 +0900

    [level 2] Title: 올바른 괄호, Time: 20.71 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length();i++) {
+            char c = s.charAt(i);
+            if(c == '(')
+                st.push(c);
+            else {
+                if(!st.isEmpty() && st.peek() == '(')
+                    st.pop();
+                else
+                    st.push(c);
+            }
+        }
+        
+        return st.isEmpty();
+    }
+}
