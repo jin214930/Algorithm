eead862a8a284780dc52a2295eac651340b71b27
Ward <91389299+jin214930@users.noreply.github.com>
2024-10-07 11:03:24 +0900

    [level 1] Title: 폰켓몬, Time: 5.69 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int ans = nums.length / 2;
+        
+        HashSet<Integer> s = new HashSet<>();
+        for(int i : nums)
+            s.add(i);
+        
+        ans = Math.min(ans, s.size());
+        return ans;
+    }
+}
