01b8e7dc1462b800aa929fee216c8e233be70a13
Ward <91389299+jin214930@users.noreply.github.com>
2024-10-12 23:31:56 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 108 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		System.out.printf("%d %d", (c * e - f * b) / (a * e - b * d), (c * d - a * f) / (b * d - a * e));
+	}
+}
