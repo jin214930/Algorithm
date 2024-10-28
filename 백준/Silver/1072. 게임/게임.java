0f21384cc9e961fa4820f4a21232b275285683a7
Ward <91389299+jin214930@users.noreply.github.com>
2024-10-28 11:23:58 +0900

    [Silver III] Title: 게임, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int z = (int) (y * 100L / x);
+		int ans = -1;
+		int s = 1, e = 1000000000;
+		while (s <= e) {
+			int m = (s + e) / 2;
+			int tmp = (int) ((y + m) * 100L / (x + m));
+			if (z < tmp) {
+				ans = m;
+				e = m - 1;
+			} else
+				s = m + 1;
+		}
+		System.out.println(ans);
+
+	}
+}
