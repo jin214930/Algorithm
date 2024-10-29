f81ea9d8849a4c50bdfa343da8829d46db6a5cde
Ward <91389299+jin214930@users.noreply.github.com>
2024-10-29 11:15:48 +0900

    [Silver III] Title: 징검다리, Time: 268 ms, Memory: 17232 KB -BaekjoonHub

---
+++ 백준/Silver/11561. 징검다리/징검다리.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			long n = Long.parseLong(br.readLine());
+			long s = 1, e = 1000000000L;
+			long ans = 0;
+			while (s <= e) {
+				long m = (s + e) / 2;
+				if (m * (m + 1) / 2 <= n) {
+					ans = m;
+					s = m + 1;
+				} else
+					e = m - 1;
+			}
+			System.out.println(ans);
+		}
+	}
+}
