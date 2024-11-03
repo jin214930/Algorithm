e6520d2ed999d76ec91c7971fa73a71089b7563d
Ward <91389299+jin214930@users.noreply.github.com>
2024-11-03 11:35:51 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 388 ms, Memory: 56116 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.java
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.java
@@ -7,34 +7,36 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		String[][] a = new String[n][2];
 		int m = Integer.parseInt(st.nextToken());
+		HashMap<String, String> mp = new HashMap<>();
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int t = Integer.parseInt(st.nextToken());
-			a[i][0] = st.nextToken();
+			String s = st.nextToken();
 			String tmp = "";
 			for (int j = 0; j < 7; j++)
 				tmp += st.nextToken();
-			a[i][1] = tmp;
+			mp.put(tmp, s);
 		}
+
 		while (m-- > 0) {
 			String s = br.readLine().replaceAll(" ", "");
 			int cnt = 0;
-			String ans = "";
-			for (int i = 0; i < n; i++) {
-				if (a[i][1].substring(0, 3).equals(s)) {
-					ans = a[i][0];
+			String tmp = "";
+			for (String i : mp.keySet()) {
+				if (i.substring(0, 3).equals(s)) {
+					tmp = i;
 					cnt++;
 				}
 			}
 			if (cnt == 0)
 				bw.write("!\n");
 			else if (cnt == 1)
-				bw.write(ans + "\n");
+				bw.write(mp.get(tmp) + "\n");
 			else
 				bw.write("?\n");
 		}
+
 		bw.flush();
 		bw.close();
 	}
