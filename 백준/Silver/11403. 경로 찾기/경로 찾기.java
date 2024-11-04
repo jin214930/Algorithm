938559d6f17d2b614dce1949a41a124ca2fddb2c
Ward <91389299+jin214930@users.noreply.github.com>
2024-11-04 11:49:49 +0900

    [Silver I] Title: 경로 찾기, Time: 192 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -8,15 +8,16 @@ public class Main {
 
 	static void dfs(int x) {
 		for (int i = 0; i < n; i++) {
-			if (visited[i] == 0 && adj[x][i] == 1) {
-				visited[i] = 1;
-				dfs(i);
-			}
+			if (visited[i] == 1 || adj[x][i] == 0)
+				continue;
+			visited[i] = 1;
+			dfs(i);
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		n = Integer.parseInt(br.readLine());
 		adj = new int[n][n];
 
@@ -30,8 +31,11 @@ public class Main {
 			visited = new int[n];
 			dfs(i);
 			for (int j = 0; j < n; j++)
-				System.out.print(visited[j] + " ");
-			System.out.println();
+				bw.write(visited[j] + " ");
+			bw.write("\n");
 		}
+
+		bw.flush();
+		bw.close();
 	}
 }
