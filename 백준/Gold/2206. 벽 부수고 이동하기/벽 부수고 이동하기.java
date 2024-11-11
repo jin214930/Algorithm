5b3f649a5f7c67243bd8e6281c05a70008542547
Ward <91389299+jin214930@users.noreply.github.com>
2024-11-11 13:38:55 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 784 ms, Memory: 137036 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -12,69 +12,52 @@ class Node {
 }
 
 public class Main {
-	static int n, m;
-	static char[][] a;
-	static int[][][] d;
 	static int[] dy = { -1, 1, 0, 0 };
 	static int[] dx = { 0, 0, -1, 1 };
-
-	static void bfs() {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0, 0, 0));
-		d[0][0][0] = 1;
-
-		while (!q.isEmpty()) {
-			Node node = q.poll();
-			for (int i = 0; i < 4; i++) {
-				int ny = node.y + dy[i];
-				int nx = node.x + dx[i];
-				if (ny < 0 || nx < 0 || ny >= n || nx >= m)
-					continue;
-				if (a[ny][nx] == '1' && node.b == 0) {
-					if (d[ny][nx][1] > d[node.y][node.x][0] + 1) {
-						d[ny][nx][1] = d[node.y][node.x][0] + 1;
-						q.add(new Node(ny, nx, 1));
-					}
-				} else if (a[ny][nx] == '0') {
-					if (node.b == 1) {
-						if (d[ny][nx][1] > d[node.y][node.x][1] + 1) {
-							d[ny][nx][1] = d[node.y][node.x][1] + 1;
-							q.add(new Node(ny, nx, 1));
-						}
-					} else {
-						if (d[ny][nx][0] > d[node.y][node.x][0] + 1) {
-							d[ny][nx][0] = d[node.y][node.x][0] + 1;
-							q.add(new Node(ny, nx, 0));
-						}
-					}
-				}
-			}
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		a = new char[n][m];
-		d = new int[n][m][2];
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		char[][] a = new char[n][m];
 		for (int i = 0; i < n; i++) {
 			String s = br.readLine();
-			for (int j = 0; j < m; j++) {
+			for (int j = 0; j < m; j++)
 				a[i][j] = s.charAt(j);
-				d[i][j][0] = 1000000000;
-				d[i][j][1] = 1000000000;
+		}
+
+		Queue<Node> q = new LinkedList<>();
+		int[][][] visited = new int[n][m][2];
+		q.add(new Node(0, 0, 0));
+		visited[0][0][0] = 1;
+
+		while (!q.isEmpty()) {
+			Node node = q.poll();
+			if (node.y == n - 1 && node.x == m - 1) {
+				bw.write(visited[n - 1][m - 1][node.b] + "");
+				bw.flush();
+				bw.close();
+				return;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int ny = node.y + dy[i];
+				int nx = node.x + dx[i];
+				if (ny < 0 || nx < 0 || ny >= n || nx >= m)
+					continue;
+				if (a[ny][nx] == '1' && node.b == 0 && visited[ny][nx][1] == 0) {
+					visited[ny][nx][1] = visited[node.y][node.x][0] + 1;
+					q.add(new Node(ny, nx, 1));
+				} else if (a[ny][nx] == '0' && visited[ny][nx][node.b] == 0) {
+					visited[ny][nx][node.b] = visited[node.y][node.x][node.b] + 1;
+					q.add(new Node(ny, nx, node.b));
+				}
 			}
 		}
 
-		bfs();
-
-		if (d[n - 1][m - 1][0] == 1000000000 && d[n - 1][m - 1][1] == 1000000000)
-			bw.write("-1");
-		else
-			bw.write(Math.min(d[n - 1][m - 1][0], d[n - 1][m - 1][1]) + "");
+		bw.write("-1");
 		bw.flush();
 		bw.close();
 	}
