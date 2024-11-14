859c2ceb64d2eb55115578167ec1d6d97084a68a
Ward <91389299+jin214930@users.noreply.github.com>
2024-11-14 20:59:11 +0900

    [level 3] Title: 단속카메라, Time: 17.46 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (o1, o2) -> {
+            if (o1[0] == o2[0])
+                return o1[1] - o2[1];
+            return o1[0] - o2[0];
+        });
+        
+        System.out.println(Arrays.deepToString(routes));
+
+        int ans = 1;
+        int tmp = routes[0][1];
+        for (int[] i : routes) {
+            if (i[0] > tmp) {
+                tmp = i[1];
+                ans++;
+            }
+            else {
+                if(i[1] < tmp)
+                    tmp = i[1];
+            }
+        }
+        
+        return ans;
+    }
+}
