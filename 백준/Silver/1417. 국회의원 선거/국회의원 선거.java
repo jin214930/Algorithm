c83b5058c2c23d9bf626a2c2d301ccef21a31aa8
Ward <91389299+jin214930@users.noreply.github.com>
2024-11-20 17:39:06 +0900

    [Silver V] Title: 국회의원 선거, Time: 112 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		int x = Integer.parseInt(br.readLine());
+		for (int i = 1; i < n; i++)
+			pq.add(Integer.parseInt(br.readLine()));
+
+		if (n == 1) {
+			bw.write("0");
+			bw.flush();
+			bw.close();
+			return;
+		}
+
+		int ans = 0;
+		while (pq.peek() >= x) {
+			x++;
+			pq.add(pq.poll() - 1);
+			ans++;
+		}
+
+		bw.write(ans + "");
+		bw.flush();
+		bw.close();
+	}
+}
