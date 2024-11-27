836cd2e5b4dcbaf0fd68efc275f04a73333b0d45
Ward <91389299+jin214930@users.noreply.github.com>
2024-11-27 21:17:27 +0900

    [Gold IV] Title: 줄세우기, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = Integer.parseInt(br.readLine());
+
+		int[] d = new int[n];
+		d[0] = 1;
+		int ans = 1;
+		for (int i = 1; i < n; i++) {
+			d[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (a[j] < a[i])
+					d[i] = Math.max(d[i], d[j] + 1);
+				ans = Math.max(ans, d[i]);
+			}
+		}
+
+		bw.write((n - ans) + "");
+		bw.flush();
+		bw.close();
+	}
+}
