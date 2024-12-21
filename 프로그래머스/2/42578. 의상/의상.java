9bd7e90b14adf98895624c3dc279d22c70fe1a84
Ward <91389299+jin214930@users.noreply.github.com>
2024-12-21 13:43:25 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,16 +4,10 @@ class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> mp = new HashMap<>();
         
-        for (String[] s : clothes) {
-            if (mp.containsKey(s[1])) {
-                mp.put(s[1], mp.get(s[1]) + 1);
-            }
-            else
-                mp.put(s[1], 1);
-        }
+        for (String[] s : clothes) 
+            mp.put(s[1], mp.getOrDefault(s[1], 0) + 1);
         
         int ans = 1;
-        
         for (int i : mp.values())
             ans *= i + 1;
         
