9a3787f04406797e45ca132cd4a3c1b2d2385951
Ward <91389299+jin214930@users.noreply.github.com>
2024-12-21 13:33:06 +0900

    [level 2] Title: 전화번호 목록, Time: 343.18 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,13 +4,8 @@ class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
         for (int i = 0; i < phone_book.length - 1; i++) {
-            String pn1 = phone_book[i];
-            String pn2 = phone_book[i + 1];
-    
-            if (pn1.length() > pn2.length() && pn2.equals(pn1.substring(0, pn2.length())))
-                return false;
-            else if (pn2.length() > pn1.length() && pn1.equals(pn2.substring(0, pn1.length())))
-                return false;
+            if (phone_book[i + 1].startsWith(phone_book[i]))
+               return false; 
         }
         
         return true;
