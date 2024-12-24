2ad5fd38fe8a33c78bae3e1affdeced5c2f6bbff
Ward <91389299+jin214930@users.noreply.github.com>
2024-12-24 12:02:07 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,22 +1,22 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] daysToComplete = new int[progresses.length];
+        int[] days = new int[progresses.length];
         for (int i = 0; i < progresses.length; i++) {
             int work = 100 - progresses[i];
-            daysToComplete[i] = (work / speeds[i]) + (work % speeds[i] == 0 ? 0 : 1);
+            days[i] = (work / speeds[i]) + (work % speeds[i] == 0 ? 0 : 1);
         }
         
         List<Integer> ansList = new ArrayList<>();
         int now = 0;
         int cnt = 0;
-        for (int i : daysToComplete) {
-            if (i <= now)
+        for (int d : days) {
+            if (d <= now)
                 cnt++;
             else {
                 if (now != 0)
                     ansList.add(cnt);
-                now = i;
+                now = d;
                 cnt = 1;
             }
         }
