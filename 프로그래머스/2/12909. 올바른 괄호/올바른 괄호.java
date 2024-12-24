b8144325c8182e419de20815bc5e6f1e39701ff8
Ward <91389299+jin214930@users.noreply.github.com>
2024-12-24 16:07:22 +0900

    [level 2] Title: 올바른 괄호, Time: 22.88 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,17 +1,16 @@
 import java.util.*;
+
 class Solution {
     boolean solution(String s) {
         Stack<Character> st = new Stack<>();
-        for(int i = 0; i < s.length();i++) {
-            char c = s.charAt(i);
-            if(c == '(')
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(')
                 st.push(c);
-            else {
-                if(!st.isEmpty() && st.peek() == '(')
-                    st.pop();
-                else
-                    st.push(c);
-            }
+            else if (!st.isEmpty() && st.peek() == '(')
+                st.pop();
+            else
+                st.push(c);
         }
         
         return st.isEmpty();
