5133cce53a10e0a8eb4799657b3feaed2e056e61
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-14 16:12:50 +0900

    [level 3] Title: 이중우선순위큐, Time: 69.43 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -4,56 +4,28 @@ class Solution {
     public int[] solution(String[] operations) {
         PriorityQueue<Integer> mnh = new PriorityQueue<>();
         PriorityQueue<Integer> mxh = new PriorityQueue<>(Collections.reverseOrder());
-        HashMap<Integer, Integer> mp = new HashMap<>();
         
         for (String s : operations) {
             char c = s.charAt(0);
-            int n = Integer.parseInt(s.substring(2));
+            int x = Integer.parseInt(s.substring(2));
+
             if (c == 'I') {
-                mnh.add(n);
-                mxh.add(n);
-                if (mp.containsKey(n))
-                    mp.put(n, mp.get(n) + 1);
-                else
-                    mp.put(n, 1);
-            }
-            else if (n == 1) {
-                while (!mxh.isEmpty()) {
-                    int x = mxh.poll();
-                    if (mp.get(x) == 0) 
-                        continue;
-                    mp.put(x, mp.get(x) - 1);
-                    break;
-                }
-            }
-            else {
-                while (!mnh.isEmpty()) {
-                    int x = mnh.poll();
-                    if (mp.get(x) == 0) 
-                        continue;
-                    mp.put(x, mp.get(x) - 1);
-                    break;
-                }
-            }
+                mnh.add(x);
+                mxh.add(x);
+            } else if (x == 1 && !mxh.isEmpty()) { 
+                mnh.remove(mxh.poll());
+            } else if (x == -1 && !mnh.isEmpty()) {
+                mxh.remove(mnh.poll());
+            } 
         }
         
         int[] ans = new int[2];
-        while (!mxh.isEmpty()) {
-            int x = mxh.poll();
-            if (mp.get(x) == 0) 
-                continue;
-            ans[0] = x;
-            break;
+        
+        if (!mxh.isEmpty()) {
+            ans[0] = mxh.peek();
+            ans[1] = mnh.peek();
         }
         
-        while (!mnh.isEmpty()) {
-            int x = mnh.poll();
-            if (mp.get(x) == 0) 
-                continue;
-            ans[1] = x;
-            break;
-        }
-            
         return ans;
     }
 }
