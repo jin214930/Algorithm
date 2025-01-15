586262ddc0ee41051fb5c345faefc35412c9b2e1
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-15 17:14:09 +0900

    [Silver IV] Title: 큐, Time: 184 ms, Memory: 20632 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,53 +1,43 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		Queue<Integer> q = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int x = 0;
-		while (n-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String s = st.nextToken();
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+        int x = -1;
 
-			switch (s) {
-				case "push":
-					x = Integer.parseInt(st.nextToken());
-					q.add(x);
-					break;
-				case "pop":
-					if (q.isEmpty())
-						bw.write("-1\n");
-					else
-						bw.write(q.poll() + "\n");
-					break;
-				case "size":
-					bw.write(q.size() + "\n");
-					break;
-				case "empty":
-					if (q.isEmpty())
-						bw.write("1\n");
-					else
-						bw.write("0\n");
-					break;
-				case "front":
-					if (q.isEmpty())
-						bw.write("-1\n");
-					else
-						bw.write(q.peek() + "\n");
-					break;
-				default:
-					if (q.isEmpty())
-						bw.write("-1\n");
-					else
-						bw.write(x + "\n");
-			}
-		}
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
 
-		bw.flush();
-		bw.close();
-	}
+            switch (cmd) {
+                case "push":
+                    x = Integer.parseInt(st.nextToken());
+                    q.add(x);
+                    break;
+                case "pop":
+                    bw.write((q.isEmpty() ? -1 : q.poll()) + "\n");
+                    break;
+                case "size":
+                    bw.write(q.size() + "\n");
+                    break;
+                case "empty":
+                    bw.write((q.isEmpty() ? 1 : 0) + "\n");
+                    break;
+                case "front":
+                    bw.write((q.isEmpty() ? -1 : q.peek()) + "\n");
+                    break;
+                default:
+                    bw.write((q.isEmpty() ? -1 : x) + "\n");
+            }
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
