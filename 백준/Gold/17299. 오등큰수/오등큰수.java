47fbde852f7ee95870c2bbb2166b95f7e5ec48b8
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-16 16:57:21 +0900

    [Gold III] Title: 오등큰수, Time: 1560 ms, Memory: 268556 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] ans = new int[n];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+            ans[i] = -1;
+            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
+        }
+
+        Stack<Integer> stk = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stk.empty() && map.get(a[stk.peek()]) < map.get(a[i]))
+                ans[stk.pop()] = a[i];
+            stk.add(i);
+        }
+
+        for (int i : ans)
+            bw.write(i + " ");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
