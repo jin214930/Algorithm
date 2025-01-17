73678f4850591fc49732057a7cc87fcd22483dab
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-17 12:21:01 +0900

    [Silver IV] Title: 접미사 배열, Time: 152 ms, Memory: 16836 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++)
+            list.add(s.substring(i));
+
+        Collections.sort(list);
+
+        for (String t : list)
+            bw.write(t + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
