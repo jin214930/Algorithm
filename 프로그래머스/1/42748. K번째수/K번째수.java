65a85932898917b2fd6edbcdaf27f8482abb23a3
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-20 09:59:55 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,14 +1,21 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] ans = new int[commands.length];
         int idx = 0;
-        for(int[] i : commands) {
-            int[] tmp = new int[i[1] - i[0] + 1];
-            for(int j = i[0] - 1; j < i[1]; j++)
-                tmp[j - i[0]+1] = array[j];
-            Arrays.sort(tmp);
-            ans[idx++] = tmp[i[2]-1];
+        
+        for (int[] command : commands) {
+            List<Integer> list = new ArrayList<>();
+            int i = command[0] - 1;
+            int j = command[1] - 1;
+            int k = command[2] - 1;
+            
+            for (int l = i; l <= j; l++)
+                list.add(array[l]);
+            
+            Collections.sort(list);
+            ans[idx++] = list.get(k);
         }
         
         return ans;
