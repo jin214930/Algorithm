6664c70393f330484060f67cbb4ebf4bd6d734f6
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-22 16:23:54 +0900

    [level 2] Title: H-Index, Time: 1.04 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
+        int ans = 0;
+        
         Arrays.sort(citations);
         
-        int ans = 0;
-        for (int i = 0; i < citations.length; i++) {
-            if (citations[i] >= citations.length - i)
-                return citations.length - i;
+        for(int i = 0; i < citations.length; i++) {
+            if (citations.length - i <= citations[i])
+                ans = Math.max(ans, citations.length - i);
         }
         
         return ans;
