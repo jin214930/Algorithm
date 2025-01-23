4719f30f9a5c4e58c0b605f11f6693a5215292a5
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-23 22:00:02 +0900

    [Silver IV] Title: GCD 합, Time: 248 ms, Memory: 22988 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(st.nextToken());
+
+            long ans = 0;
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < i; j++)
+                    ans += gcd(a[i], a[j]);
+            }
+
+            bw.write(ans + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
