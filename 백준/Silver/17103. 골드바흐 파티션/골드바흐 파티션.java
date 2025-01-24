44876081dff72f117084d52df947f0407c0f0c81
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-24 16:30:02 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 288 ms, Memory: 17520 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        boolean[] p = new boolean[1000001];
+        p[1] = true;
+
+        for (int i = 2; i <= 1000000; i++) {
+            if (p[i]) continue;
+            for (int j = i * 2; j <= 1000000; j += i)
+                p[j] = true;
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int ans = 0;
+            for (int i = 2; i <= n / 2; i++) {
+                if (!p[i] && !p[n - i])
+                    ans++;
+            }
+            bw.write(ans + "\n");
+        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
