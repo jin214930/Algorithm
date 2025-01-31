b9772e83b8f609228c8a993bb48d09a502aab870
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-31 14:30:12 +0900

    [level 1] Title: 모의고사, Time: 1.84 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,35 +1,41 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] a = { { 1, 2, 3, 4, 5 }, 
-                     { 2, 1, 2, 3, 2, 4, 2, 5 }, 
-                     { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } 
-                    };
-
+        int[][] pattern = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5}, 
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        
         int[] cnt = new int[3];
+        
         for (int i = 0; i < 3; i++) {
             int idx = 0;
-            for (int j : answers) {
-                if (idx == a[i].length)
-                    idx = 0;
-                if (j == a[i][idx++])
+            for (int answer : answers) {
+                if (answer == pattern[i][idx])
                     cnt[i]++;
+                idx = (idx + 1) % pattern[i].length;
             }
         }
         
-        ArrayList<Integer> al = new ArrayList<>();
         int mx = 0;
         for (int i : cnt)
-            mx = Math.max(i, mx);
+            mx = Math.max(mx, i);
+        
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            if (cnt[i] == mx)
-                al.add(i + 1);
+            if (mx == cnt[i])
+                list.add(i + 1);
         }
         
+        Collections.sort(list);
+        
+        int[] ans = new int[list.size()];
         int idx = 0;
-        int[] ans = new int[al.size()];
-        for (int i : al)
+        for (int i : list)
             ans[idx++] = i;
-        return ans;
+        
+        return ans;        
     }
 }
