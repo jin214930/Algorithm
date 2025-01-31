4d13ffe9b576ce10a2e23d9662ad4b82e3323693
Ward <91389299+jin214930@users.noreply.github.com>
2025-01-31 16:41:52 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] d = new int[101][10];
+        for (int i = 1; i < 10; i++)
+            d[1][i] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            d[i][0] = d[i - 1][1];
+            for (int j = 1; j < 9; j++)
+                d[i][j] = (d[i - 1][j - 1] + d[i - 1][j + 1]) % 1000000000;
+            d[i][9] = d[i - 1][8];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 10; i++)
+            ans = (ans + d[n][i]) % 1000000000;
+
+        bw.write(ans + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
