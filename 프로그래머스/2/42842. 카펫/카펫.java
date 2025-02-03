55a14df4936b0620a4a6225ea7048b1abf8f6332
Ward <91389299+jin214930@users.noreply.github.com>
2025-02-03 11:16:36 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,10 @@
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] ans = new int[2];
-        
+    public int[] solution(int brown, int yellow) {        
         for (int i = 1; i * i <= yellow; i++) {
-            if (yellow % i == 0) {
-                int tmp = (i + yellow / i) * 2 + 4;
-                if (tmp == brown) {
-                    ans[0] = yellow / i + 2;
-                    ans[1] = i + 2;
-                    return ans;
-                }
-            }
+            if (yellow % i == 0 && (i + yellow / i) * 2 + 4 == brown) 
+                    return new int[]{yellow / i + 2, i + 2};
         }
         
-        return ans;
+        return null;
     }
 }
