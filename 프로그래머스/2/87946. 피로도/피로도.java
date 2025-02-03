9818c534d583a7b337466bbe7d803029e44b78d2
Ward <91389299+jin214930@users.noreply.github.com>
2025-02-03 12:10:18 +0900

    [level 2] Title: 피로도, Time: 3.58 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,13 +1,13 @@
 class Solution {
-    int ans = 0;
+    int ans;
     boolean[] visited;
     
-    void go(int cnt, int k, int[][] a) {
+    void go(int cnt, int k, int[][] dungeons) {
         ans = Math.max(ans, cnt);
-        for (int i = 0; i < a.length; i++) {
-            if (!visited[i] && a[i][0] <= k) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
-                go(cnt + 1,k - a[i][1], a);
+                go(cnt + 1, k - dungeons[i][1], dungeons);
                 visited[i] = false;
             }
         }
@@ -16,6 +16,7 @@ class Solution {
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
         go(0, k, dungeons);
+        
         return ans;
     }
 }
