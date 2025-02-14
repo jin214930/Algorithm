5432e4532ef0a03e35a83da2c6d71596f37da74e
Ward <91389299+jin214930@users.noreply.github.com>
2025-02-14 17:15:20 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] tmp = new int[n + 1];
-        Arrays.fill(tmp, 1);
+        int[] a = new int[n + 1];
+        Arrays.fill(a, 1);
         
         for (int i : lost)
-            tmp[i]--;
+            a[i]--;
         for (int i : reserve)
-            tmp[i]++;
+            a[i]++;
         
         for (int i = 1; i <= n; i++) {
-            if (tmp[i] == 0) {
-                if (i >= 2 && tmp[i - 1] == 2) {
-                    tmp[i - 1] = 1;
-                    tmp[i] = 1;
-                } else if (i <= n - 1 && tmp[i + 1] == 2) {
-                    tmp[i + 1] = 1;
-                    tmp[i] = 1;
+            if (a[i] == 0) {
+                if (i >= 2 && a[i - 1] == 2) {
+                    a[i - 1] = 1;
+                    a[i] = 1;
+                } else if (i <= n - 1 && a[i + 1] == 2) {
+                    a[i + 1] = 1;
+                    a[i] = 1;
                 }
             }
         }
         
         int ans = 0;
         for (int i = 1; i <= n ;i++) {
-            if (tmp[i] >= 1)
+            if (a[i] >= 1)
                 ans++;
         }
         
