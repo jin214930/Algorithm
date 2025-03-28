5e5421f100dde8aa3f211ab76e93fddcd037faa6
Ward <91389299+jin214930@users.noreply.github.com>
2025-03-28 16:33:20 +0900

    [Gold V] Title: 괄호의 값, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        Stack<Character> st = new Stack<>();
+        int ans = 0;
+        int tmp = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                st.push(c);
+                tmp *= 2;
+            } else if (c == '[') {
+                st.push(c);
+                tmp *= 3;
+            } else if (c == ')' && !st.empty() && st.peek() == '(') {
+                if (i > 0 && s.charAt(i - 1) == '(')
+                    ans += tmp;
+                st.pop();
+                tmp /= 2;
+            } else if (c == ']' && !st.empty() && st.peek() == '[') {
+                if (i > 0 && s.charAt(i - 1) == '[')
+                    ans += tmp;
+                st.pop();
+                tmp /= 3;
+            } else
+                st.push(c);
+        }
+
+        if (!st.empty())
+            bw.write("0");
+        else
+            bw.write(ans + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
