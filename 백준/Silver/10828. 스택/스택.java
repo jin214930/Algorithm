bf286a2325cf9cb894ed9ee3b1354e3f690f2cfc
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-19 10:52:27 +0900

    [Silver IV] Title: 스택, Time: 180 ms, Memory: 20080 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,39 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		Stack<Integer> stk = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		while (n-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String s = st.nextToken();
-			if (s.equals("push"))
-				stk.push(Integer.parseInt(st.nextToken()));
-			else if (s.equals("pop")) {
-				if (stk.isEmpty())
-					bw.write("-1\n");
-				else
-					bw.write(stk.pop() + "\n");
-			} else if (s.equals("size"))
-				bw.write(stk.size() + "\n");
-			else if (s.equals("empty")) {
-				if (stk.isEmpty())
-					bw.write("1\n");
-				else
-					bw.write("0\n");
-			} else {
-				if (stk.isEmpty())
-					bw.write("-1\n");
-				else
-					bw.write(stk.peek() + "\n");
-			}
-		}
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stk = new Stack<>();
 
-		bw.flush();
-		bw.close();
-	}
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String cmd = st.nextToken();
+            switch (cmd) {
+                case "push":
+                    int x = Integer.parseInt(st.nextToken());
+                    stk.push(x);
+                    break;
+                case "pop":
+                    bw.write((stk.isEmpty() ? -1 : stk.pop()) + "\n");
+                    break;
+                case "size":
+                    bw.write(stk.size() + "\n");
+                    break;
+                case "empty":
+                    bw.write((stk.isEmpty() ? 1 : 0) + "\n");
+                    break;
+                default:
+                    bw.write((stk.isEmpty() ? -1 : stk.peek()) + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
