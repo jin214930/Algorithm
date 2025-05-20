b9934dde77f5f2205eec049d63ca1b89c96ffee3
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-20 16:01:13 +0900

    [Gold II] Title: 후위 표기식, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -1,12 +1,41 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Deque<Character> st = new ArrayDeque<>();
+
+        String s = br.readLine();
+
+        for (char c : s.toCharArray()) {
+            if (c >= 'A' && c <= 'Z')
+                bw.write(c);
+            else {
+                if (c == '+' || c == '-') {
+                    while (!st.isEmpty() && st.peek() != '(')
+                        bw.write(st.pop());
+                    st.push(c);
+                } else if (c == '*' || c == '/') {
+                    while (!st.isEmpty() && (st.peek() != '+' && st.peek() != '-' && st.peek() != '('))
+                        bw.write(st.pop());
+                    st.push(c);
+                } else if (c == '(')
+                    st.push(c);
+                else {
+                    while (!st.isEmpty() && st.peek() != '(')
+                        bw.write(st.pop());
+                    st.pop();
+                }
+            }
+        }
+
+        while (!st.isEmpty())
+            bw.write(st.pop());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
