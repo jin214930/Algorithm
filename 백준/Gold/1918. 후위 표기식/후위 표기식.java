5b03ed6eecce429fb37e20b7a1381a33289ba0dc
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-20 15:53:06 +0900

    [Gold II] Title: 후위 표기식, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -1,40 +1,12 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String s = br.readLine();
-        Stack<Character> st = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (c >= 'A' && c <= 'Z')
-                bw.write(c);
-            else if (c == '+' || c == '-') {
-                while (!st.empty() && st.peek() != '(')
-                    bw.write(st.pop());
-                st.push(c);
-            } else if (c == '*' || c == '/') {
-                if (!st.empty() && (st.peek() == '*' || st.peek() == '/'))
-                    bw.write(st.pop());
-                st.push(c);
-            } else if (c == '(') {
-                st.push(c);
-            } else {
-                while (st.peek() != '(')
-                    bw.write(st.pop());
-                st.pop();
-            }
-        }
-
-        while (!st.empty())
-            bw.write(st.pop());
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-}
-
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
