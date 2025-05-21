73337f12e8ad6251330202e38e4c4c4932b0c628
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-21 10:38:13 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 21920 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,13 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         boolean[] p = new boolean[1000001];
         p[1] = true;
-
         for (int i = 2; i <= 1000000; i++) {
             if (!p[i]) {
                 for (int j = i * 2; j <= 1000000; j += i)
@@ -17,10 +22,10 @@ public class Main {
         }
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        for (int i = m; i <= n; i++) {
+        for (int i = a; i <= b; i++) {
             if (!p[i])
                 bw.write(i + "\n");
         }
@@ -30,4 +35,3 @@ public class Main {
         br.close();
     }
 }
-
