a98a89597f1a05fe32ffdf1b3e358e31fcc95792
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-21 10:36:41 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -2,13 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         boolean[] p = new boolean[1001];
         p[1] = true;
-
         for (int i = 2; i <= 1000; i++) {
             if (!p[i]) {
                 for (int j = i * 2; j <= 1000; j += i)
@@ -19,17 +24,16 @@ public class Main {
         int ans = 0;
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        while (n-- > 0) {
-            int tmp = Integer.parseInt(st.nextToken());
-            if (!p[tmp])
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if (!p[x])
                 ans++;
         }
 
         bw.write(ans + "");
+
         bw.flush();
         bw.close();
         br.close();
     }
 }
-
