3fc8a6848e99c5ad90cfa8b1df5799bf9f6de27d
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-22 17:10:31 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 284 ms, Memory: 17352 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -8,14 +8,15 @@ public class Main {
 
         boolean[] p = new boolean[1000001];
         p[1] = true;
-
         for (int i = 2; i <= 1000000; i++) {
-            if (p[i]) continue;
-            for (int j = i * 2; j <= 1000000; j += i)
-                p[j] = true;
+            if (!p[i]) {
+                for (int j = i * 2; j <= 1000000; j += i)
+                    p[j] = true;
+            }
         }
 
         int t = Integer.parseInt(br.readLine());
+
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
             int ans = 0;
@@ -23,6 +24,7 @@ public class Main {
                 if (!p[i] && !p[n - i])
                     ans++;
             }
+
             bw.write(ans + "\n");
         }
 
