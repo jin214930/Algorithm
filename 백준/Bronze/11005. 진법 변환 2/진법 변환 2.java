86e42ff7e43f99c42c837368dbd8ac3dc9c415cd
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-22 17:18:01 +0900

    [Bronze I] Title: 진법 변환 2, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -5,15 +5,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        StringBuilder sb = new StringBuilder();
         while (n != 0) {
             int tmp = n % b;
             if (tmp >= 10)
-                sb.append((char) (tmp - 10 + 'A'));
+                sb.append((char) ('A' + tmp - 10));
             else
                 sb.append(tmp);
             n /= b;
