80e9bc8834a80107e72e6861636005393acd0dd7
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-23 11:03:55 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -6,16 +6,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-
         int[] d = new int[1001];
         d[1] = 1;
         d[2] = 3;
 
+        int n = Integer.parseInt(br.readLine());
+
         for (int i = 3; i <= n; i++)
-            d[i] = (d[i - 1] + d[i - 2] * 2) % 10007;
+            d[i] = (d[i - 1] + 2 * d[i - 2]) % 10007;
 
         bw.write(d[n] + "");
+
         bw.flush();
         bw.close();
         br.close();
