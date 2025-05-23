86c416882b6ad50c94543b382fceaa8e53e88a36
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-23 11:11:44 +0900

    [Silver I] Title: 카드 구매하기, Time: 132 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -11,16 +11,17 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++)
             p[i] = Integer.parseInt(st.nextToken());
-
         int[] d = new int[1001];
         d[1] = p[1];
+
         for (int i = 2; i <= n; i++) {
             d[i] = p[i];
             for (int j = 1; j < i; j++)
-                d[i] = Math.max(d[i], d[j] + p[i - j]);
+                d[i] = Math.max(d[i], d[i - j] + p[j]);
         }
 
         bw.write(d[n] + "");
+
         bw.flush();
         bw.close();
         br.close();
