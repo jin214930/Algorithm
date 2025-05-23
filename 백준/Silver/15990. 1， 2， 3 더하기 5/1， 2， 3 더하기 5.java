ef3511ac05194ad1d5d9de11c5cd4c9c01707cbd
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-23 11:37:35 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 160 ms, Memory: 20384 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -6,24 +6,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
-
         int[][] d = new int[100001][4];
         d[1][1] = 1;
         d[2][2] = 1;
         d[3][1] = 1;
         d[3][2] = 1;
         d[3][3] = 1;
+
         for (int i = 4; i <= 100000; i++) {
             d[i][1] = (d[i - 1][2] + d[i - 1][3]) % 1000000009;
             d[i][2] = (d[i - 2][1] + d[i - 2][3]) % 1000000009;
             d[i][3] = (d[i - 3][1] + d[i - 3][2]) % 1000000009;
         }
 
+        int t = Integer.parseInt(br.readLine());
+
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
             bw.write(((d[n][1] + d[n][2]) % 1000000009 + d[n][3]) % 1000000009 + "\n");
         }
+
 
         bw.flush();
         bw.close();
