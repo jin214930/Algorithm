a38a4abecbee23d19117854dc1295db0283a0b9f
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-23 11:48:40 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -6,17 +6,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-
         long[][] d = new long[91][2];
         d[1][1] = 1;
 
+        int n = Integer.parseInt(br.readLine());
+
         for (int i = 2; i <= n; i++) {
-            d[i][1] = d[i - 1][0];
             d[i][0] = d[i - 1][1] + d[i - 1][0];
+            d[i][1] = d[i - 1][0];
         }
 
-        bw.write(d[n][0] + d[n][1] + "");
+        bw.write((d[n][0] + d[n][1]) + "");
+
         bw.flush();
         bw.close();
         br.close();
