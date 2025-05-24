57217c9d16276d19d86aeeba459e0044d0f3a2b5
Ward <91389299+jin214930@users.noreply.github.com>
2025-05-24 13:06:42 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 128 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,30 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		int[] a = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++)
-			a[i] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int[] d = new int[n];
-		d[0] = 1;
-		int ans = 1;
-		for (int i = 1; i < n; i++) {
-			d[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (a[j] > a[i])
-					d[i] = Math.max(d[i], d[j] + 1);
-			}
-			ans = Math.max(ans, d[i]);
-		}
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(st.nextToken());
 
-		bw.write(ans + "");
-		bw.flush();
-		bw.close();
-	}
+        int[] d = new int[n];
+        d[0] = 1;
+        int ans = 1;
+        for (int i = 1; i < n; i++) {
+            d[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (a[j] > a[i])
+                    d[i] = Math.max(d[i], d[j] + 1);
+            }
+            ans = Math.max(ans, d[i]);
+        }
+
+        bw.write(ans + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
