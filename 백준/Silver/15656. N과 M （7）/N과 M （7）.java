44a39db22f87062785b3c9704a0596d229e70cdd
Ward <91389299+jin214930@users.noreply.github.com>
2025-06-19 10:04:37 +0900

    [Silver III] Title: N과 M (7), Time: 652 ms, Memory: 162968 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] a, b;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static void go(int idx) throws IOException {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                bw.write(b[i] + " ");
+            }
+            bw.write("\n");
+
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            b[idx] = a[i];
+            go(idx + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        a = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(a);
+
+        b = new int[m];
+
+        go(0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
