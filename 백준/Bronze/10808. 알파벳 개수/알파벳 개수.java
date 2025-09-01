e8117ff77aba9c99f2ba333487a3bc451bf1c61f
Ward <91389299+jin214930@users.noreply.github.com>
2025-09-01 11:42:42 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 112 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,12 +1,15 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int[] cnt = new int[26];
+
+		for (int i = 0; i < s.length(); i++)
+			cnt[s.charAt(i) - 'a']++;
+		for (int i = 0; i < 26; i++)
+			System.out.print(cnt[i] + " ");
+	}
+}
