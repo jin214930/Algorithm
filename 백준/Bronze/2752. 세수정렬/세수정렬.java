d1000e2d9072721361cbc0f5155e7f70fd34fc25
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-25 20:06:57 +0900

    [Bronze IV] Title: 세수정렬, Time: 120 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] a = new int[3];
+        for (int i = 0; i < 3; i++)
+            a[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(a);
+
+        for (int i : a)
+            bw.write(i + " ");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
