9b863fe176c4db56fd7fe842fc6086af0f316be7
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-25 21:17:00 +0900

    [Bronze II] Title: 카드 역배치, Time: 108 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.java
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.java
@@ -5,21 +5,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int a[] = new int[21];
 
+        int[] a = new int[21];
         for (int i = 1; i <= 20; i++)
             a[i] = i;
 
         for (int i = 0; i < 10; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            while (y > x) {
-                int temp = a[x];
-                a[x] = a[y];
-                a[y] = temp;
-                x++;
-                y--;
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < (e - s + 1) / 2; j++) {
+                int tmp = a[s + j];
+                a[s + j] = a[e - j];
+                a[e - j] = tmp;
             }
         }
 
