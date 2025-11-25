97c42dc937918adbc2f3eaa926cc11a768c91ef5
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-25 20:45:39 +0900

    [Bronze II] Title: 숫자, Time: 260 ms, Memory: 21996 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.java
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -1,22 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long a = Long.parseLong(st.nextToken());
-		long b = Long.parseLong(st.nextToken());
-		if(a > b) {
-			long tmp = a;
-			a = b;
-			b = tmp;
-		}
-		bw.write(Math.max(b - a - 1, 0) + "\n");
-		for (long i = a + 1; i < b; i++)
-			bw.write(i + " ");
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long[] a = new long[2];
+        for (int i = 0; i < 2; i++)
+            a[i] = Long.parseLong(st.nextToken());
+
+        Arrays.sort(a);
+
+        if (a[1] - a[0] <= 1)
+            bw.write("0");
+        else {
+            bw.write((a[1] - a[0] - 1) + "\n");
+            for (long i = a[0] + 1; i < a[1]; i++)
+                bw.write(i + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
