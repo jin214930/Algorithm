f0115872497682f0a931939812ce39e3bc4fd2bc
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-26 04:07:22 +0900

    [Silver II] Title: 스택 수열, Time: 308 ms, Memory: 27076 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -5,27 +5,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Deque<Integer> st = new ArrayDeque<>();
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         int[] a = new int[n];
-
         for (int i = 0; i < n; i++)
             a[i] = Integer.parseInt(br.readLine());
 
+        Stack<Integer> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         int idx = 0;
         for (int i = 1; i <= n; i++) {
             st.push(i);
             sb.append("+\n");
             while (!st.isEmpty() && st.peek() == a[idx]) {
+                sb.append("-\n");
                 st.pop();
                 idx++;
-                sb.append("-\n");
             }
         }
 
-				bw.write(st.isEmpty() ? sb.toString() : "NO");
+        if (!st.isEmpty())
+            bw.write("NO");
+        else
+            bw.write(sb.toString());
 
         bw.flush();
         bw.close();
