022b6c26060de4f1f6a123cf0cfa7774f5b0fd43
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-25 23:29:59 +0900

    [Bronze II] Title: 숫자의 개수, Time: 116 ms, Memory: 15756 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,24 +1,29 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int a = Integer.parseInt(br.readLine());
-		int b = Integer.parseInt(br.readLine());
-		int c = Integer.parseInt(br.readLine());
-		int tmp = a * b * c;
-		int[] cnt = new int[10];
-		while (tmp != 0) {
-			cnt[tmp % 10]++;
-			tmp /= 10;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		for (int i : cnt)
-			bw.write(i + "\n");
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
 
-		bw.flush();
-		bw.close();
-	}
+        int ret = a * b * c;
+
+        int[] cnt = new int[10];
+        while (ret > 0) {
+            cnt[ret % 10]++;
+            ret /= 10;
+        }
+
+        for (int i : cnt)
+            bw.write(i + "\n");
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
