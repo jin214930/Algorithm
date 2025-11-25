e38c536da3885fa6e7e8bd578eea29c3007e3129
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-25 23:46:33 +0900

    [Bronze II] Title: 방 배정, Time: 116 ms, Memory: 14640 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -5,25 +5,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] cnt = new int[2][7];
 
+        int[][] cnt = new int[7][2];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            cnt[s][y]++;
+
+            cnt[y][s]++;
         }
 
         int ans = 0;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 1; j <= 6; j++)
-                ans += cnt[i][j] / k + (cnt[i][j] % k > 0 ? 1 : 0);
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 0; j < 2; j++)
+                ans += cnt[i][j] / k + (cnt[i][j] % k == 0 ? 0 : 1);
         }
 
         bw.write(ans + "");
+        
         bw.flush();
         bw.close();
         br.close();
