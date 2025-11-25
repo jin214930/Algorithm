d6e2b5d73587ca7b6a1e29bce5f4271dd3866abc
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-25 20:39:42 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 116 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,29 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] a = new int[9];
-		int sum = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		for (int i = 0; i < 9; i++) {
-			a[i] = Integer.parseInt(br.readLine());
-			sum += a[i];
-		}
+        int[] a = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+            sum += a[i];
+        }
 
-		Arrays.sort(a);
+        Arrays.sort(a);
 
-		for (int i = 1; i < 9; i++) {
-			for (int j = 0; j < i; j++) {
-				if (sum - a[i] - a[j] == 100) {
-					for (int k = 0; k < 9; k++) {
-						if (k != i && k != j)
-							System.out.println(a[k]);
-					}
-					return;
-				}
-			}
-		}
-	}
+        for (int i = 1; i < 9; i++) {
+            for (int j = 0; j < i; j++) {
+                if (sum - a[i] - a[j] == 100) {
+                    for (int k = 0; k < 9; k++) {
+                        if (k != i && k != j)
+                            bw.write(a[k] + "\n");
+                    }
+                    bw.flush();
+                    bw.close();
+                    br.close();
+                    return;
+                }
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
