03cc8383f29aee2c7b86dfd2565961a802cd9af0
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-25 21:24:55 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 116 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
 
+        int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 2 * n - i - 1; j++) {
-                if (j < i)
-                    bw.write(" ");
-                else
+                if (j >= i && j <= 2 * n - 1 - i)
                     bw.write("*");
+                else
+                    bw.write(" ");
             }
             bw.write("\n");
         }
