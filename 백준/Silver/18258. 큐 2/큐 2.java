368d8c3ab98225578d1af8305b1c1bbe58e44c5a
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-26 14:23:22 +0900

    [Silver IV] Title: 큐 2, Time: 1240 ms, Memory: 348364 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+        int x = 0;
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "push":
+                    x = Integer.parseInt(st.nextToken());
+                    q.add(x);
+                    break;
+                case "pop":
+                    bw.write((q.isEmpty() ? -1 : q.poll()) + "\n");
+                    break;
+                case "size":
+                    bw.write(q.size() + "\n");
+                    break;
+                case "empty":
+                    bw.write((q.isEmpty() ? 1 : 0) + "\n");
+                    break;
+                case "front":
+                    bw.write((q.isEmpty() ? -1 : q.peek()) + "\n");
+                    break;
+                case "back":
+                    bw.write((q.isEmpty() ? -1 : x) + "\n");
+            }
+        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
