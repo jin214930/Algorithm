40928e65c8c6a7ecd936fb6a3f3b736e9e8976e8
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-26 12:14:27 +0900

    [Gold IV] Title: 오큰수, Time: 920 ms, Memory: 151276 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -5,29 +5,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Deque<Integer> stk = new ArrayDeque<>();
 
         int n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
-        int[] ans = new int[n];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
             a[i] = Integer.parseInt(st.nextToken());
-            ans[i] = -1;
-        }
 
+        Stack<Integer> stk = new Stack<>();
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
         for (int i = 0; i < n; i++) {
-            while (!stk.isEmpty() && a[stk.peek()] < a[i]) {
-                ans[stk.peek()] = a[i];
-                stk.pop();
-            }
-
+            while (!stk.isEmpty() && a[stk.peek()] < a[i])
+                ans[stk.pop()] = a[i];
             stk.push(i);
         }
 
-        for (int i = 0; i < n; i++)
-            bw.write(ans[i] + " ");
+        for (int i : ans)
+            bw.write(i + " ");
 
         bw.flush();
         bw.close();
