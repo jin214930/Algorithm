beeee9f2fb9150175f020125fff0f85e71265ea2
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-26 14:44:20 +0900

    [Silver III] Title: 회전하는 큐, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -5,26 +5,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        Deque<Integer> deque = new LinkedList<>();
+
+        Deque<Integer> dq = new ArrayDeque<>();
         for (int i = 1; i <= n; i++)
-            deque.addLast(i);
-        int ans = 0;
+            dq.add(i);
+
         st = new StringTokenizer(br.readLine());
+        int ans = 0;
         while (m-- > 0) {
-            int cnt = 0;
             int x = Integer.parseInt(st.nextToken());
-            while (!deque.isEmpty() && deque.peekFirst() != x) {
-                deque.addLast(deque.pollFirst());
+            int cnt = 0;
+            while (dq.peek() != x) {
                 cnt++;
+                dq.addLast(dq.pollFirst());
             }
-            deque.pollFirst();
-            ans += Math.min(cnt, deque.size() - cnt + 1);
+            ans += Math.min(cnt, dq.size() - cnt);
+            dq.pollFirst();
         }
 
         bw.write(ans + "");
+
         bw.flush();
         bw.close();
         br.close();
