44e7bc94d682101ca0800a70b9fbf76847ece5cf
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-26 17:11:13 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16404 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Deque<Character> st = new ArrayDeque<>();
 
-        int ans = 0;
         String s = br.readLine();
 
+        int ans = 0;
+        Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(')
