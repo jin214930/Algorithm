c807e781b94ebbc44c6184e754151b91fbc5fb0c
Ward <91389299+jin214930@users.noreply.github.com>
2025-11-27 12:40:30 +0900

    [Gold V] Title: 토마토, Time: 672 ms, Memory: 131996 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,75 +1,75 @@
-import java.util.*;
 import java.io.*;
-
-class Node {
-	int z, y, x;
-
-	public Node(int z, int y, int x) {
-		this.z = z;
-		this.y = y;
-		this.x = x;
-	}
-}
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int m = Integer.parseInt(st.nextToken());
-		int n = Integer.parseInt(st.nextToken());
-		int h = Integer.parseInt(st.nextToken());
-		int[] dz = { 1, -1, 0, 0, 0, 0 };
-		int[] dy = { 0, 0, 1, -1, 0, 0 };
-		int[] dx = { 0, 0, 0, 0, 1, -1 };
-		int[][][] a = new int[h][n][m];
-		int[][][] visited = new int[h][n][m];
-		Queue<Node> q = new LinkedList<>();
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < n; j++) {
-				st = new StringTokenizer(br.readLine());
-				for (int k = 0; k < m; k++) {
-					a[i][j][k] = Integer.parseInt(st.nextToken());
-					if (a[i][j][k] == 1) {
-						q.add(new Node(i, j, k));
-						visited[i][j][k] = 1;
-					}
-				}
-			}
-		}
+    static class Node {
+        int z, y, x;
 
-		while (!q.isEmpty()) {
-			Node node = q.poll();
-			for (int i = 0; i < 6; i++) {
-				int nz = node.z + dz[i];
-				int ny = node.y + dy[i];
-				int nx = node.x + dx[i];
-				if (nz < 0 || ny < 0 || nx < 0 || nz >= h || ny >= n || nx >= m)
-					continue;
-				if (visited[nz][ny][nx] >= 1 || a[nz][ny][nx] == -1)
-					continue;
-				q.add(new Node(nz, ny, nx));
-				visited[nz][ny][nx] = visited[node.z][node.y][node.x] + 1;
-			}
-		}
+        Node(int z, int y, int x) {
+            this.z = z;
+            this.y = y;
+            this.x = x;
+        }
+    }
 
-		int ans = 0;
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int k = 0; k < m; k++) {
-					if (visited[i][j][k] == 0 && a[i][j][k] != -1) {
-						bw.write("-1");
-						bw.flush();
-						bw.close();
-						return;
-					}
-					ans = Math.max(ans, visited[i][j][k]);
-				}
-			}
-		}
+    static int[] dy = {-1, 1, 0, 0, 0, 0};
+    static int[] dx = {0, 0, -1, 1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
 
-		bw.write((ans - 1) + "");
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int[][][] a = new int[h][n][m];
+        int[][][] visited = new int[h][n][m];
+        Queue<Node> q = new LinkedList<>();
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < m; k++) {
+                    a[i][j][k] = Integer.parseInt(st.nextToken());
+                    if (a[i][j][k] == 1) {
+                        q.add(new Node(i, j, k));
+                        visited[i][j][k] = 1;
+                    }
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            for (int i = 0; i < 6; i++) {
+                int nz = node.z + dz[i];
+                int ny = node.y + dy[i];
+                int nx = node.x + dx[i];
+                if (nz < 0 || ny < 0 || nx < 0 || nz >= h || ny >= n || nx >= m || a[nz][ny][nx] != 0 || visited[nz][ny][nx] != 0)
+                    continue;
+                q.add(new Node(nz, ny, nx));
+                visited[nz][ny][nx] = visited[node.z][node.y][node.x] + 1;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < m; k++) {
+                    if (a[i][j][k] == 0 && visited[i][j][k] == 0)
+                        ans = Integer.MAX_VALUE;
+                    else
+                        ans = Math.max(ans, visited[i][j][k]);
+                }
+            }
+        }
+
+        bw.write((ans == Integer.MAX_VALUE ? -1 : ans - 1) + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
