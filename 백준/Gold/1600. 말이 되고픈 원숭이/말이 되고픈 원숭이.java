bf0a0a02e6f87f6a71030eda3fd5f40a44bf3b3b
Ward <91389299+jin214930@users.noreply.github.com>
2025-12-01 17:57:30 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 676 ms, Memory: 89856 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,72 +1,76 @@
-import java.util.*;
 import java.io.*;
-
-class Node {
-	int y, x, k;
-
-	public Node(int y, int x, int k) {
-		this.y = y;
-		this.x = x;
-		this.k = k;
-	}
-}
+import java.util.*;
 
 public class Main {
-	static int[] dy = { -1, 1, 0, 0 };
-	static int[] dx = { 0, 0, -1, 1 };
-	static int[] dyh = { -2, -1, 1, 2, 2, 1, -1, -1 };
-	static int[] dxh = { 1, 2, 2, 1, -1, -2, -2, -1 };
+    static class Node {
+        int y, x, h;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int k = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int m = Integer.parseInt(st.nextToken());
-		int n = Integer.parseInt(st.nextToken());
-		int[][] a = new int[n][m];
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < m; j++)
-				a[i][j] = Integer.parseInt(st.nextToken());
-		}
+        Node(int y, int x, int h) {
+            this.y = y;
+            this.x = x;
+            this.h = h;
+        }
+    }
 
-		int[][][] visited = new int[n][m][k + 1];
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0, 0, 0));
-		visited[0][0][0] = 1;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dyH = {-1, -2, -2, -1, 1, 2, 2, 1};
+    static int[] dxH = {-2, -1, 1, 2, 2, 1, -1, -2};
 
-		while (!q.isEmpty()) {
-			Node node = q.poll();
-			if (node.y == n - 1 && node.x == m - 1) {
-				bw.write((visited[node.y][node.x][node.k] - 1) + "");
-				bw.flush();
-				bw.close();
-				return;
-			}
-			for (int i = 0; i < 4; i++) {
-				int ny = node.y + dy[i];
-				int nx = node.x + dx[i];
-				if (ny < 0 || nx < 0 || ny >= n || nx >= m || a[ny][nx] == 1 || visited[ny][nx][node.k] >= 1)
-					continue;
-				q.add(new Node(ny, nx, node.k));
-				visited[ny][nx][node.k] = visited[node.y][node.x][node.k] + 1;
-			}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-			if (node.k < k) {
-				for (int i = 0; i < 8; i++) {
-					int ny = node.y + dyh[i];
-					int nx = node.x + dxh[i];
-					if (ny < 0 || nx < 0 || ny >= n || nx >= m || a[ny][nx] == 1 || visited[ny][nx][node.k + 1] >= 1)
-						continue;
-					q.add(new Node(ny, nx, node.k + 1));
-					visited[ny][nx][node.k + 1] = visited[node.y][node.x][node.k] + 1;
-				}
-			}
-		}
+        int k = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
 
-		bw.write("-1");
-		bw.flush();
-		bw.close();
-	}
+        int[][] a = new int[h][w];
+        for (int i = 0; i < h; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < w; j++)
+                a[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        int[][][] visited = new int[h][w][k + 1];
+        q.add(new Node(0, 0, 0));
+        visited[0][0][0] = 1;
+
+        int ans = -1;
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            if (node.y == h - 1 && node.x == w - 1) {
+                ans = visited[node.y][node.x][node.h] - 1;
+                break;
+            }
+
+            if (node.h < k) {
+                for (int i = 0; i < 8; i++) {
+                    int ny = node.y + dyH[i];
+                    int nx = node.x + dxH[i];
+                    if (ny < 0 || nx < 0 || ny >= h || nx >= w) continue;
+                    if (visited[ny][nx][node.h + 1] != 0 || a[ny][nx] == 1) continue;
+                    q.add(new Node(ny, nx, node.h + 1));
+                    visited[ny][nx][node.h + 1] = visited[node.y][node.x][node.h] + 1;
+                }
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int ny = node.y + dy[i];
+                int nx = node.x + dx[i];
+                if (ny < 0 || nx < 0 || ny >= h || nx >= w) continue;
+                if (visited[ny][nx][node.h] != 0 || a[ny][nx] == 1) continue;
+                q.add(new Node(ny, nx, node.h));
+                visited[ny][nx][node.h] = visited[node.y][node.x][node.h] + 1;
+            }
+        }
+
+        bw.write(ans + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
