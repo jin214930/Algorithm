3f9310e7c1adef6de35ba53a98090e80cd2a3cc2
Ward <91389299+jin214930@users.noreply.github.com>
2025-12-04 00:22:09 +0900

    [Silver I] Title: 쿼드트리, Time: 112 ms, Memory: 14956 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -4,44 +4,42 @@ import java.util.*;
 public class Main {
     static char[][] a;
 
-    static String go(int y, int x, int s) {
-        if (s == 1) {
-            if (a[y][x] == '0')
-                return "0";
-            else
-                return "1";
-        }
+    static String go(int y, int x, int n) {
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < s; i++) {
-            for (int j = 0; j < s; j++) {
-                if (a[y][x] != a[y + i][x + j]) {
-                    StringBuilder sb = new StringBuilder();
+        char tmp = a[y][x];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tmp != a[y + i][x + j]) {
                     sb.append("(");
-                    sb.append(go(y, x, s / 2));
-                    sb.append(go(y, x + s / 2, s / 2));
-                    sb.append(go(y + s / 2, x, s / 2));
-                    sb.append(go(y + s / 2, x + s / 2, s / 2));
+                    sb.append(go(y, x, n / 2));
+                    sb.append(go(y, x + n / 2, n / 2));
+                    sb.append(go(y + n / 2, x, n / 2));
+                    sb.append(go(y + n / 2, x + n / 2, n / 2));
                     sb.append(")");
                     return sb.toString();
                 }
             }
         }
 
-        if (a[y][x] == '0')
-            return "0";
-        else
-            return "1";
+        sb.append(tmp);
+        return sb.toString();
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
+
         a = new char[n][n];
         for (int i = 0; i < n; i++)
             a[i] = br.readLine().toCharArray();
 
         bw.write(go(0, 0, n));
+
         bw.flush();
+        bw.close();
+        br.close();
     }
 }
