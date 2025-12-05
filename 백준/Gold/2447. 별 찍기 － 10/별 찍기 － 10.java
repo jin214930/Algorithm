38de7dcd91ee13fd230646e1455ced5310fbea69
Ward <91389299+jin214930@users.noreply.github.com>
2025-12-05 22:04:37 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 412 ms, Memory: 141548 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -2,28 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static char[][] a = new char[2500][2500];
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        go(0, 0, n);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (a[i][j] != '*')
-                    bw.write(' ');
-                else
-                    bw.write(a[i][j]);
-            }
-            bw.write("\n");
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+    static char[][] a;
 
     static void go(int y, int x, int n) {
         if (n == 1) {
@@ -32,11 +11,36 @@ public class Main {
         }
 
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3;j++) {
-                if(i == 1 && j == 1)
-                    continue;
-                go(y + i * n / 3, x + j * n / 3, n / 3);
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) continue;
+                go(y + n / 3 * i, x + n / 3 * j, n / 3);
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        a = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                a[i][j] = ' ';
+        }
+
+
+        go(0, 0, n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                bw.write(a[i][j] + "");
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
