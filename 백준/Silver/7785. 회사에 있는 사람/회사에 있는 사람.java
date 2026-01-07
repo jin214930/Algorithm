77dc0697c27d750faeaac3fec1bc7deba6b98997
Ward <91389299+jin214930@users.noreply.github.com>
2026-01-08 00:46:49 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 692 ms, Memory: 53300 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,28 +1,31 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		HashSet<String> s = new HashSet<>();
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String a = st.nextToken();
-			String b = st.nextToken();
-			if (b.equals("enter"))
-				s.add(a);
-			else
-				s.remove(a);
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String[] ans = s.toArray(new String[0]);
-		Arrays.sort(ans, Comparator.reverseOrder());
-		for (String i : ans)
-			bw.write(i + "\n");
+        int n = Integer.parseInt(br.readLine());
 
-		bw.flush();
-		bw.close();
-	}
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            String cmd = st.nextToken();
+
+            if (cmd.equals("enter"))
+                set.add(s);
+            else
+                set.remove(s);
+        }
+
+        List<String> list = new ArrayList<>(set);
+        Collections.sort(list, Collections.reverseOrder());
+        for (String s : list)
+            bw.write(s + "\n");
+
+
+        bw.flush();
+    }
 }
