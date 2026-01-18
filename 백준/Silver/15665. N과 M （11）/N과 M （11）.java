dbf642bb82f000fb0d92b8241d543fe36817b674
Ward <91389299+jin214930@users.noreply.github.com>
2026-01-18 20:44:17 +0900

    [Silver II] Title: N과 M (11), Time: 708 ms, Memory: 153828 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -2,49 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static int[] a;
-    static Set<List<Integer>> ans;
+	static int n, m;
+	static int[] a;
+	static Set<List<Integer>> set = new LinkedHashSet<>();
 
-    static void go(List<Integer> list) {
-        if (list.size() == m) {
-            ans.add(new ArrayList<>(list));
-            return;
-        }
+	static void go(List<Integer> list) {
+		if (list.size() == m) {
+			set.add(new ArrayList<>(list));
+			return;
+		}
 
-        for (int i = 0; i < n; i++) {
-            list.add(a[i]);
-            go(list);
-            list.remove(list.size() - 1);
-        }
-    }
+		for (int i = 0; i < n; i++) {
+			list.add(a[i]);
+			go(list);
+			list.remove(list.size() - 1);
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        a = new int[n];
-        ans = new LinkedHashSet<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++)
-            a[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(a);
+		a = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			a[i] = Integer.parseInt(st.nextToken());
 
-        go(new ArrayList<>());
+		Arrays.sort(a);
 
-        for (List<Integer> list : ans) {
-            for (int i : list) {
-                bw.write(i + " ");
-            }
-            bw.write("\n");
-        }
+		go(new ArrayList<>());
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		for (List<Integer> list : set) {
+			for (int i : list)
+				bw.write(i + " ");
+			bw.write("\n");
+		}
+
+		bw.flush();
+	}
 }
