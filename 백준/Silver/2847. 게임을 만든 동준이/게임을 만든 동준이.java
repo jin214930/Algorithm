3fc04c4971cd3eee2181410e87c1ab5ec7421a71
Ward <91389299+jin214930@users.noreply.github.com>
2026-02-01 18:27:03 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 112 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -1,25 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int n = Integer.parseInt(br.readLine());
-		int a[] = new int[n];
+		int[] a = new int[n];
 		for (int i = 0; i < n; i++)
 			a[i] = Integer.parseInt(br.readLine());
 
 		int ans = 0;
-		for (int i = n - 2; i >= 0; i--) {
-			if (a[i + 1] <= a[i]) {
-				ans += a[i] - a[i + 1] + 1;
-				a[i] = a[i + 1] - 1;
+		for (int i = 1; i < n; i++) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (a[j + 1] <= a[j]) {
+					ans += (a[j] - a[j + 1] + 1);
+					a[j] = a[j + 1] - 1;
+				}
 			}
 		}
 
 		bw.write(ans + "");
 		bw.flush();
-		bw.close();
 	}
 }
