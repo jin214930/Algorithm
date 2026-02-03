cf8a93800b8a482d312f876c57551e2255b6af89
Ward <91389299+jin214930@users.noreply.github.com>
2026-02-03 19:23:03 +0900

    [Gold IV] Title: 강의실 배정, Time: 628 ms, Memory: 69472 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,34 +1,51 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	static class Class {
+		int s, t;
+
+		public Class(int s, int t) {
+			this.s = s;
+			this.t = t;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int n = Integer.parseInt(br.readLine());
-		int[][] a = new int[n][2];
+		Class[] classes = new Class[n];
+
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			a[i][0] = Integer.parseInt(st.nextToken());
-			a[i][1] = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			classes[i] = new Class(s, t);
 		}
 
-		Arrays.sort(a, (o1, o2) -> o1[0] - o2[0]);
+		Arrays.sort(classes, (c1, c2) -> {
+			if (c1.s == c2.s) {
+				return c1.t - c2.t;
+			}
+			return c1.s - c2.s;
+		});
 
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		int ans = 0;
-		for (int[] i : a) {
-			if (pq.isEmpty() || pq.peek() > i[0]) {
-				pq.add(i[1]);
-				ans++;
-			} else {
-				pq.poll();
-				pq.add(i[1]);
-			}
-		}
+		int ans = 1;
+		pq.add(classes[0].t);
 
-		bw.write(ans + "");
+		for (int i = 1; i < n; i++) {
+			if (pq.peek() <= classes[i].s) {
+				pq.poll();
+			} else {
+				ans++;
+			}
+			pq.add(classes[i].t);
+		}
+		
+		bw.write(ans + "\n");
 		bw.flush();
-		bw.close();
 	}
 }
