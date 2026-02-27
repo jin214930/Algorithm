d94a31e18870c409bcb9c667e518d6b0960769a5
Ward <91389299+jin214930@users.noreply.github.com>
2026-02-28 08:05:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.75 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,20 +4,16 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         
-        for (String c : completion) {
-            if (map.containsKey(c))
-                map.put(c, map.get(c) + 1);
+        for(String s : completion)
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        
+        for (String s : participant) {
+            if (!map.containsKey(s) || map.get(s) == 0)
+                return s;
             else
-                map.put(c, 1);
+                map.put(s, map.get(s) - 1);            
         }
         
-        for (String p : participant) {
-            if (map.containsKey(p) && map.get(p) > 0) 
-                map.put(p, map.get(p) - 1);
-            else
-                return p;
-        }
-        
-        return "";
+        return null;        
     }
 }
