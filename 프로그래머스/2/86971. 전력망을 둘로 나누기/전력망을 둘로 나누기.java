b6e4f978cb8d0732667925ce0ea6dcf46f90aa9f
Ward <91389299+jin214930@users.noreply.github.com>
2026-03-02 11:12:14 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.90 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,42 +1,41 @@
 import java.util.*;
 
 class Solution {
-    List<List<Integer>> adj;
+    List<Integer>[] adj;
     boolean[] visited;
     
-    public int dfs(int x) {
-        int cnt = 1;
+    int dfs(int x, int y) {
+        int tmp = 1;
         visited[x] = true;
-        for(int nx : adj.get(x)) {
-            if (!visited[nx])
-               cnt += dfs(nx); 
+        for (int nx : adj[x]) {
+            if (visited[nx]) continue;
+            if (nx == y) continue;
+            tmp += dfs(nx, y);
         }
         
-        return cnt;
+        return tmp;
     }
     
     public int solution(int n, int[][] wires) {
-        int ans = 1000000000;
+        adj = new ArrayList[n + 1];
         
-        for (int i = 0; i < wires.length; i++) {
-            adj = new ArrayList<>();
-            for (int j = 0; j <= n; j++)
-                adj.add(new ArrayList<>());
+        for(int i = 0; i <= n; i++)
+            adj[i] = new ArrayList<>();
+        
+        for(int[] wire : wires) {
+            adj[wire[0]].add(wire[1]);
+            adj[wire[1]].add(wire[0]);
+        }
+        
+        int ans = 100000;
+        
+        for (int[] wire : wires) {
             visited = new boolean[n + 1];
+            int tmp1 = dfs(wire[0], wire[1]);
+            visited = new boolean[n + 1];
+            int tmp2 = dfs(wire[1], wire[0]);
             
-            for (int j = 0; j < wires.length; j++) {
-                if (i == j) continue;
-                adj.get(wires[j][0]).add(wires[j][1]);
-                adj.get(wires[j][1]).add(wires[j][0]);
-            }
-            
-            int[] tmp = new int[2];
-            int idx = 0;
-            for (int j = 1; j <= n; j++) {
-                if (!visited[j]) 
-                    tmp[idx++] = dfs(j);
-            }
-            ans = Math.min(ans, Math.abs(tmp[0] - tmp[1]));
+            ans = Math.min(ans, Math.abs(tmp1 - tmp2));
         }
         
         return ans;
