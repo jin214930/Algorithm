127364662ae74e01c9b89d222d52b7c26b0e6b8c
Ward <91389299+jin214930@users.noreply.github.com>
2026-03-04 17:05:19 +0900

    [level 3] Title: 순위, Time: 7.62 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,30 +1,45 @@
+import java.util.*;
+
 class Solution {
-    static boolean[] visited;
-    static int[] cnt;
-    int dfs(int x, int[][] a) {
+    boolean[] visited;
+    int[] cnt;
+    List<Integer>[] adj;
+    
+    int dfs(int x) {
         visited[x] = true;
         int tmp = 1;
-        for(int[] i : a) {
-            if(i[0] != x)
-                continue;
-            if(visited[i[1]])
-                continue;
-            cnt[i[1]]++;
-            tmp += dfs(i[1] ,a);
+        
+        for (int nx : adj[x]) {
+            if (visited[nx]) continue;
+            
+            cnt[nx]++;
+            tmp += dfs(nx);
         }
+        
         return tmp;
     }
+    
     public int solution(int n, int[][] results) {
         cnt = new int[n + 1];
-        for(int i = 1; i <=n;i++) {
+        adj = new ArrayList[n + 1];
+        
+        for (int i = 0; i <= n; i++)
+            adj[i] = new ArrayList<>();
+        
+        for(int[] edge : results)
+            adj[edge[0]].add(edge[1]);
+        
+        for(int i = 1; i <= n; i++) {
             visited = new boolean[n + 1];
-            cnt[i] += dfs(i, results);
+            cnt[i] += dfs(i);
         }
+        
         int ans = 0;
-        for(int i = 1; i <=n;i++) {
-            if(n == cnt[i])
+        for(int i = 1; i <=n; i++) {
+            if (n == cnt[i])
                 ans++;
         }
-        return ans;
+        
+        return ans;        
     }
 }
