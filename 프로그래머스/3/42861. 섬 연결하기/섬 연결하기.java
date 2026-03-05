9d1caccd2843286d2fc0753a070463192fe46b36
Ward <91389299+jin214930@users.noreply.github.com>
2026-03-05 19:23:08 +0900

    [level 3] Title: 섬 연결하기, Time: 0.39 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
-class Solution {
+class Solution {    
     public int solution(int n, int[][] costs) {
         int ans = 0;
         Set<Integer> set = new HashSet<>();
         set.add(0);
         
-        while (set.size() != n) {
-            int mn = 1000000000;
-            int nx = -1;
+        while(set.size() != n) {
+            int mn = Integer.MAX_VALUE;
+            int x = -1;
             for (int[] cost : costs) {
                 if (set.contains(cost[0]) && !set.contains(cost[1])) {
                     if (mn > cost[2]) {
                         mn = cost[2];
-                        nx = cost[1];
+                        x = cost[1];
                     }
-                } 
+                }
                 
-                if (set.contains(cost[1]) && !set.contains(cost[0])) {
+                if (!set.contains(cost[0]) && set.contains(cost[1])) {
                     if (mn > cost[2]) {
                         mn = cost[2];
-                        nx = cost[0];
+                        x = cost[0];
                     }
-                } 
+                }
             }
             
-            set.add(nx);
-            ans += mn;
+            set.add(x);
+            ans += mn;         
         }
         
-        return ans;        
+        return ans;
     }
 }
