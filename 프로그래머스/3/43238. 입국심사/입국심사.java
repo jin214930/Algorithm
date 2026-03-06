b0e4c326fc57cace9d36923a67715ead3169230c
Ward <91389299+jin214930@users.noreply.github.com>
2026-03-06 11:37:04 +0900

    [level 3] Title: 입국심사, Time: 59.01 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,19 +1,27 @@
 class Solution {
+    boolean check(long m, int n, int[] times) {
+        long cnt = 0;
+        for (int time : times) {
+            cnt += m / time;
+        }
+        
+        return cnt >= n;
+    }
+    
     public long solution(int n, int[] times) {
+        long s = 1L;
+        long e = 1000000000000000000L;
+        
         long ans = 0;
-        long s = 1, e = (long)1e18;
         while(s <= e) {
             long m = (s + e) / 2;
-            long cnt = 0;
-            for(int i : times)
-                cnt += m / i;
-            if(cnt >= n) {
+            if(check(m, n, times)) {
                 ans = m;
-                e = m -1;
-            }
-            else
+                e = m - 1;
+            } else
                 s = m + 1;
         }
+        
         return ans;
     }
 }
