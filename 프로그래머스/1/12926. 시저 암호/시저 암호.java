d90627285e30d4e096ef622492a7968b49d06221
Ward <91389299+jin214930@users.noreply.github.com>
2026-03-08 12:05:38 +0900

    [level 1] Title: 시저 암호, Time: 0.82 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            if (c == ' ')
+                sb.append(c);
+            else {
+                if (c >= 'a' && c <= 'z') {
+                    if (c + n > 'z')
+                        sb.append((char)(c + n - 'z' + 'a' - 1));
+                    else
+                        sb.append((char)(c + n));
+                } else {
+                    if (c + n > 'Z')
+                        sb.append((char)(c + n - 'Z' + 'A' - 1));
+                    else
+                        sb.append((char)(c + n));
+                    
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
