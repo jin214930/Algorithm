2fadf278965aeeba879cb32e794717dcef5171e0
Ward <91389299+jin214930@users.noreply.github.com>
2026-03-09 02:39:47 +0900

    [level 2] Title: 모음 사전, Time: 24.50 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,25 +1,27 @@
-class Solution {
-    char[] vow = {'A', 'E', 'I', 'O', 'U'};
-    int ans;
-    boolean flag;
-    
-    void go(int idx, String s, String word) {
-        if (s.equals(word)) {
-            flag = true;
-            return;
-        }
-        if (s.length() == 5) return;
-        
-        for (int i = idx; i < 5; i++) {
-            if (flag) return;
-            ans++;
-            go(idx, s + vow[i], word);
-        }
-    }
+import java.util.*;
 
-    public int solution(String word) {
-        go(0, "", word);
+class Solution {
+    List<String> list = new ArrayList<>();
+    char[] vowel = {'A','E', 'I', 'O', 'U'};
+    
+    void go(String s) {
+        if (s.length() > 5) return;
         
-        return ans;
+        if (s.length() != 0)
+            list.add(s);
+        
+        for (int i = 0; i < 5; i++)
+            go(s + vowel[i]);
+    }
+    
+    public int solution(String word) {
+        go("");
+        
+        for(int i = 0; i < list.size(); i++) {
+            if (word.equals(list.get(i)))
+                return i + 1;
+        }
+        
+        return -1;
     }
 }
