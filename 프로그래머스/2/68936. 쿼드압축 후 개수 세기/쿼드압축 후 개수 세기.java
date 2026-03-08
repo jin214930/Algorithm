74a578623310718b1b625db71b2c6d621bcacec2
Ward <91389299+jin214930@users.noreply.github.com>
2026-03-09 02:45:57 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 13.12 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[] ans = new int[2];
+    
+    void go(int y, int x, int n, int[][] arr) {
+        if (n == 1) {
+            ans[arr[y][x]]++;
+            return;
+        }
+        
+        int tmp = arr[y][x];
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tmp != arr[y + i][x + j]) {
+                    go(y, x, n / 2, arr);
+                    go(y + n / 2, x, n/2, arr);
+                    go(y, x + n/2, n/2, arr);
+                    go(y + n/2,x+n/2, n/2,arr);
+                    return;
+                }
+            }
+        }
+        ans[tmp]++;
+    }
+    
+    public int[] solution(int[][] arr) {
+        go(0, 0, arr.length, arr);
+        
+        return ans;
+    }
+}
