766469e968fa91d51271deffd248eca2c134db23
Ward <rlawlsaud2149@gmail.com>
2026-03-11 16:24:34 +0900

    [level 1] Title: 옹알이 (2), Time: 0.23 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,29 +1,42 @@
-import java.util.*;
-
 class Solution {
-    Set<String> set = new HashSet<>();
-    String[] tmp = {"aya", "ye", "woo", "ma"};
-    
     public int solution(String[] babbling) {
-        go("", -1);
-        
         int ans = 0;
-        for (String s : babbling) {
-            if (set.contains(s))
+        
+        for(String s : babbling) {
+            if (check(s)) {
                 ans++;
-        } 
+            }
+        }
         
         return ans;
     }
     
-    public void go(String s, int idx) {
-        if (s.length() > 30)
-            return;
-        set.add(s);
-        
-        for (int i = 0; i < 4; i++) {
-            if (idx != i)
-                go(s + tmp[i], i);
+    boolean check(String s) {
+        if (s.length() == 1) {
+            return false;
         }
+        
+        String prev = "";
+        for (int i = 0; i < s.length(); ) {
+            char c = s.charAt(i);
+            
+            if (c == 'a' && i + 2 < s.length() && s.substring(i, i + 3).equals("aya") && !prev.equals("aya")) {
+                prev = s.substring(i, i + 3);
+                i += 3;
+            } else if (c == 'y' && i + 1 < s.length() && s.substring(i, i + 2).equals("ye") && !prev.equals("ye")) {
+                prev = s.substring(i, i + 2);
+                i += 2;
+            } else if (c == 'w' && i + 2 < s.length() && s.substring(i, i + 3).equals("woo") && !prev.equals("woo")) {
+                prev = s.substring(i, i + 3);
+                i += 3;
+            } else if (c == 'm' && i + 1 < s.length() && s.substring(i, i + 2).equals("ma") && !prev.equals("ma")) {
+                prev = s.substring(i, i + 2);
+                i += 2;
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
