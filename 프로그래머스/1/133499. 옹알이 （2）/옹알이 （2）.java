cc3163c8de948fa8ea616146bf9ad8435a5fff60
Ward <rlawlsaud2149@gmail.com>
2026-03-11 16:24:51 +0900

    [level 1] Title: 옹알이 (2), Time: 0.23 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -21,17 +21,17 @@ class Solution {
             char c = s.charAt(i);
             
             if (c == 'a' && i + 2 < s.length() && s.substring(i, i + 3).equals("aya") && !prev.equals("aya")) {
-                prev = s.substring(i, i + 3);
                 i += 3;
+                prev = s.substring(i, i + 3);
             } else if (c == 'y' && i + 1 < s.length() && s.substring(i, i + 2).equals("ye") && !prev.equals("ye")) {
-                prev = s.substring(i, i + 2);
                 i += 2;
+                prev = s.substring(i, i + 2);
             } else if (c == 'w' && i + 2 < s.length() && s.substring(i, i + 3).equals("woo") && !prev.equals("woo")) {
-                prev = s.substring(i, i + 3);
                 i += 3;
+                prev = s.substring(i, i + 3);
             } else if (c == 'm' && i + 1 < s.length() && s.substring(i, i + 2).equals("ma") && !prev.equals("ma")) {
-                prev = s.substring(i, i + 2);
                 i += 2;
+                prev = s.substring(i, i + 2);
             } else {
                 return false;
             }
