a150b004462c77b8ec7bab8e7591093f5099999f
Ward <rlawlsaud2149@gmail.com>
2026-03-12 11:39:33 +0900

    [level 2] Title: 피로도, Time: 18.51 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,38 @@
+import java.util.*;
+
 class Solution {
     int ans = 0;
     boolean[] visited;
     
-    void go(int k, int[][] dungeons, int cnt) {
-        ans = Math.max(ans, cnt);
-        for(int i = 0; i < dungeons.length; i++) {
-            if (visited[i]) continue;
-            if (dungeons[i][0] > k) continue;
-            
-            visited[i] = true;
-            go(k - dungeons[i][1], dungeons, cnt + 1);
-            visited[i] = false;            
-        }
-    }
-    
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
         
-        go(k, dungeons, 0);
+        go(dungeons, k, new ArrayList<>());
         
         return ans;
+    }
+    
+    void go(int[][] dungeons, int k, List<Integer> list) {
+        if (list.size() == dungeons.length) {
+            int cnt = 0;
+            for (int i : list) {
+                if (dungeons[i][0] > k) break;
+                cnt++;
+                k -= dungeons[i][1];
+            }
+            
+            ans = Math.max(ans, cnt);
+            
+            return;
+        }
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            list.add(i);
+            go(dungeons, k, list);
+            visited[i] = false;
+            list.remove(list.size() - 1);
+        }
     }
 }
