4bf7fdb7da8b4d1c773890b3c4a8f747e63e7a07
Ward <rlawlsaud2149@gmail.com>
2026-03-13 10:15:23 +0900

    [level 2] Title: 교점에 별 만들기, Time: 15.86 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,56 +1,25 @@
-/*
-1. 모든 직선의 교점을 담는다.
-2. 교점 중 x, y의 최대 최소를 구함
-3. result로 표현
-*/
-
 import java.util.*;
 
 class Solution {
     class Point {
         long x, y;
-
+        
         Point(long x, long y) {
             this.x = x;
             this.y = y;
         }
     }
     
-    Point get(int[] l1, int[] l2) {
-        long a1 = l1[0], b1 = l1[1], c1 = l1[2];
-        long a2 = l2[0], b2 = l2[1], c2 = l2[2];
-        
-        long denom = a2 * b1 - a1 * b2;
-        
-        if (denom == 0)
-            return null;
-        
-        long xNum = b1 * c2 - b2 * c1;
-        long yNum = a1 * c2 - a2 * c1;
-        
-        if (xNum % denom != 0 || yNum % denom != 0)
-            return null;
-        
-        return new Point(xNum / -denom, yNum / denom);
-    }
+    List<Point> list = new ArrayList<>();
     
     public String[] solution(int[][] line) {
-        int n = line.length;
-        
-        List<Point> list = new ArrayList<>();
-        
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = i + 1; j < n; j++) {
-                Point p = get(line[i], line[j]);
-                if (p != null)
-                    list.add(p);
-            }
+        for (int i = 0; i < line.length - 1; i++) {
+            for (int j = i + 1; j < line.length; j++)
+                getPoint(line[i], line[j]);
         }
         
-        long minX = Long.MAX_VALUE;
-        long maxX = Long.MIN_VALUE;
-        long minY = Long.MAX_VALUE;
-        long maxY = Long.MIN_VALUE;
+        long minX = Long.MAX_VALUE, minY = Long.MAX_VALUE;
+        long maxX = Long.MIN_VALUE, maxY = Long.MIN_VALUE;
         
         for (Point p : list) {
             minX = Math.min(minX, p.x);
@@ -67,8 +36,8 @@ class Solution {
             Arrays.fill(map[i], '.');
         
         for (Point p : list) {
-            int x = (int) (p.x - minX);
             int y = (int) (maxY - p.y);
+            int x = (int) (p.x - minX);
             map[y][x] = '*';
         }
         
@@ -77,6 +46,19 @@ class Solution {
             ans[i] = new String(map[i]);
         
         return ans;
+    }
+    
+    void getPoint(int[] l1, int[] l2) {
+        long a1 = l1[0], b1 = l1[1], c1 = l1[2];
+        long a2 = l2[0], b2 = l2[1], c2 = l2[2];
         
+        long denom = a2 * b1 - a1 * b2;
+        if(denom == 0) return;
+        
+        long numX = b1 * c2 - b2 * c1;
+        long numY = a1 * c2 - a2 * c1;
+
+        if (numX % denom == 0 && numY % denom == 0)
+            list.add(new Point(numX / -denom, numY / denom));
     }
 }
