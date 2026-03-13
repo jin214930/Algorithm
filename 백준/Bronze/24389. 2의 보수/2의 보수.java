2f92b181b5132aa1eabdc9ce3bb0be245726345f
Ward <rlawlsaud2149@gmail.com>
2026-03-13 11:05:38 +0900

    [Bronze I] Title: 2의 보수, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/24389. 2의 보수/2의 보수.java
+++ 백준/Bronze/24389. 2의 보수/2의 보수.java
@@ -7,22 +7,8 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
-
-		int tmp = ~n + 1;
-
-		String s1 = Integer.toBinaryString(n);
-		String s2 = Integer.toBinaryString(tmp);
-
-		while (s1.length() != 32)
-			s1 = "0" + s1;
-		while (s2.length() != 32)
-			s2 = "0" + s2;
-
-		int ans = 0;
-		for (int i = 0; i < 32; i++) {
-			if (s1.charAt(i) != s2.charAt(i))
-				ans++;
-		}
+		
+		int ans = Integer.bitCount(n ^ (-n));		
 
 		bw.write(ans + "");
 
