0b95e65c2e4ac97b6bce35bfc62c8e5a7a47df62
Ward <rlawlsaud2149@gmail.com>
2026-03-14 19:38:30 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 86.96 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,67 +1,49 @@
-import java.util.*;
-
-class Solution {    
+class Solution {
+    int[][][] cnt = new int[101][101][300];
+    
     public int solution(int[][] points, int[][] routes) {
-        int n = routes.length;
         
-        List<Integer>[] path = new ArrayList[n];
-        int max = 0;
-        
-        for (int i = 0; i < n; i++) {
-            path[i] = new ArrayList<>();
-            
-            int r = points[routes[i][0] - 1][0];
-            int c = points[routes[i][0] - 1][1];
-            path[i].add(r * 1000 + c);
-            
-            for (int j = 1; j < routes[i].length; j++) {
-                int nr = points[routes[i][j] - 1][0];
-                int nc = points[routes[i][j] - 1][1];
-                
-                while (r != nr || c != nc) {
-                    if (r != nr) {
-                        if (r < nr) r++;
-                        else r--;
-                    } else {
-                        if (c < nc) c++;
-                        else c--;
-                    }
-                    
-                    path[i].add(r * 1000 + c);
-                }
-            }
-            
-            max = Math.max(max, path[i].size());
+        for (int i = 0; i < routes.length; i++) {
+            check(points[routes[i][0] - 1], points[routes[i][1] - 1]);
         }
         
         int ans = 0;
-        int[][] map = new int[101][101];
-        
-        for (int t = 0; t < max; t++) {
-            for (int i = 0; i < n; i++) {
-                if (t < path[i].size()) {
-                    int pos = path[i].get(t);
-                    int r = pos / 1000;
-                    int c = pos % 1000;
-                    
-                    map[r][c]++;
-                    
-                    if (map[r][c] == 2)
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                for (int k = 0; k < 300; k++) {
+                    if (cnt[i][j][k] >= 2) {
                         ans++;
-                }
-            }
-            
-            for (int i = 0; i < n; i++) {
-                if (t < path[i].size()) {
-                    int pos = path[i].get(t);
-                    int r = pos / 1000;
-                    int c = pos % 1000;
-                    map[r][c] = 0;
+                    }
                 }
             }
         }
         
         return ans;
     }
-
+    
+    void check(int[] s, int[] e) {
+        int t = 0;
+        
+        int r = s[0], c = s[1];
+        
+        while (!(r == e[0] && c == e[1])) {
+            cnt[r][c][t]++;
+            
+            if (r == e[0]) {
+                if (c > e[1])
+                    c--;
+                else
+                    c++;
+            } else {
+                if (r > e[0])
+                    r--;
+                else
+                    r++;
+            }
+            
+            t++;
+        }
+        
+        cnt[r][c][t]++;
+    }
 }
