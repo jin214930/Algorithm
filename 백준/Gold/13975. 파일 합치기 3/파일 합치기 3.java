f528f4cd00d0c7614e05b5b160c69ad59e533374
Ward <rlawlsaud2149@gmail.com>
2026-03-17 19:01:28 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2588 ms, Memory: 281228 KB -BaekjoonHub

--- 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -1,29 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int t = Integer.parseInt(br.readLine());
+
 		while (t-- > 0) {
 			int n = Integer.parseInt(br.readLine());
+
 			PriorityQueue<Long> pq = new PriorityQueue<>();
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < n; i++)
+			while (n-- > 0) {
 				pq.add(Long.parseLong(st.nextToken()));
+			}
 
 			long ans = 0;
-			while (pq.size() > 1) {
-				long a = pq.poll();
-				long b = pq.poll();
-				ans += a + b;
-				pq.add(a + b);
+			while (pq.size() != 1) {
+				long x1 = pq.poll();
+				long x2 = pq.poll();
+				ans += x1 + x2;
+				pq.add(x1 + x2);
 			}
 
 			bw.write(ans + "\n");
 		}
+
 		bw.flush();
-		bw.close();
 	}
 }
