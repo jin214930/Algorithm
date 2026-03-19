e2ff55b68aae5cb3301fc0988cd1d7cc03e57d67
Ward <rlawlsaud2149@gmail.com>
2026-03-19 15:34:56 +0900

    [Bronze II] Title: 농구 경기, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.java
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -1,26 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] cnt = new int[26];
-		boolean flag = true;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		int n = Integer.parseInt(br.readLine());
+
+		int[] cnt = new int[26];
 		for (int i = 0; i < n; i++) {
 			String s = br.readLine();
 			cnt[s.charAt(0) - 'a']++;
 		}
 
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 26; i++) {
 			if (cnt[i] >= 5) {
-				flag = false;
-				System.out.print((char) (i + 'a'));
+				sb.append((char)(i + 'a'));
 			}
 		}
 
-		if (flag)
-			System.out.println("PREDAJA");
+		if (sb.length() == 0)
+			bw.write("PREDAJA");
+		else
+			bw.write(sb.toString());
+		bw.flush();
 	}
 }
