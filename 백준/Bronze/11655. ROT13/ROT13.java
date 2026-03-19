d800fd6a9fd61095a9bb19772c0af25d87a55415
Ward <rlawlsaud2149@gmail.com>
2026-03-19 15:43:17 +0900

    [Bronze I] Title: ROT13, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -2,25 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String s = br.readLine();
-        StringBuilder sb = new StringBuilder();
+		String s = br.readLine();
+		StringBuilder sb = new StringBuilder();
+		for (char c : s.toCharArray()) {
+			if (c >= 'a' && c < 'n' || c >= 'A' && c < 'N')
+				sb.append((char)(c + 13));
+			else if (c >= 'n' && c <= 'z' || c >= 'N' && c <= 'Z')
+				sb.append((char)(c - 13));
+			else
+				sb.append(c);
+		}
 
-        for (char c : s.toCharArray()) {
-            if (c >= 'n' && c <= 'z' || c >= 'N' && c <= 'Z')
-                sb.append((char) (c - 13));
-            else if (c >= 'a' && c < 'n' || c >= 'A' && c < 'N')
-                sb.append((char) (c + 13));
-            else
-                sb.append(c);
-        }
+		bw.write(sb.toString());
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.flush();
+	}
 }
