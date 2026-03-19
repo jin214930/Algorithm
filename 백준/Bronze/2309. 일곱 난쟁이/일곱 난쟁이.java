06ab160a301669cf4873e8d14a41a8d75d965993
Ward <rlawlsaud2149@gmail.com>
2026-03-19 13:49:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,36 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] a = new int[9];
-        int sum = 0;
-        for (int i = 0; i < 9; i++) {
-            a[i] = Integer.parseInt(br.readLine());
-            sum += a[i];
-        }
+		int[] a = new int[9];
+		int sum = 0;
+		for (int i = 0; i < 9; i++) {
+			a[i] = Integer.parseInt(br.readLine());
+			sum += a[i];
+		}
 
-        Arrays.sort(a);
+		Arrays.sort(a);
 
-        for (int i = 1; i < 9; i++) {
-            for (int j = 0; j < i; j++) {
-                if (sum - a[i] - a[j] == 100) {
-                    for (int k = 0; k < 9; k++) {
-                        if (k != i && k != j)
-                            bw.write(a[k] + "\n");
-                    }
-                    bw.flush();
-                    bw.close();
-                    br.close();
-                    return;
-                }
-            }
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		for (int i = 0; i < 8; i++) {
+			for (int j = i + 1; j < 9; j++) {
+				if (sum - a[i] - a[j] == 100) {
+					for (int k = 0; k < 9; k++) {
+						if (k == i || k == j)
+							continue;
+						bw.write(a[k] + "\n");
+					}
+					bw.flush();
+					return;
+				}
+			}
+		}
+	}
 }
