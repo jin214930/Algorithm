71fb2838948f9fc488a15c141daa920555798b36
Ward <rlawlsaud2149@gmail.com>
2026-03-20 13:36:44 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16488 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -2,29 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		int[] a = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			a[i] = Integer.parseInt(st.nextToken());
-		}
+       int n = Integer.parseInt(br.readLine());
+       int m = Integer.parseInt(br.readLine());
+       int[] a = new int[n];
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       for (int i = 0; i < n; i++) {
+          a[i] = Integer.parseInt(st.nextToken());
+       }
 
-		int ans = 0;
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = i + 1; j < n; j++) {
-				if (a[i] + a[j] == m) {
-					ans++;
-				}
-			}
-		}
+       Arrays.sort(a);
 
-		bw.write(ans + "");
+       int ans = 0;
+       int s = 0;
+       int e = n - 1;
 
-		bw.flush();
-	}
+       while (s < e) {
+          int sum = a[s] + a[e];
+          
+          if (sum == m) {
+             ans++;
+             s++;
+             e--;
+          } else if (sum < m) {
+             s++;
+          } else {
+             e--;
+          }
+       }
+
+       bw.write(ans + "");
+
+       bw.flush();
+       bw.close();
+       br.close();
+    }
 }
