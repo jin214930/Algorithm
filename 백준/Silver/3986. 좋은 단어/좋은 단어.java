f35a414aba626c2e8dcfa87998b27f252a7e5a54
Ward <rlawlsaud2149@gmail.com>
2026-03-20 13:42:22 +0900

    [Silver IV] Title: 좋은 단어, Time: 268 ms, Memory: 23144 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -2,32 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int ans = 0;
+		while (n-- > 0) {
+			String s = br.readLine();
+			Stack<Character> st = new Stack<>();
 
-        int ans = 0;
-        while (n-- > 0) {
-            String s = br.readLine();
+			for (char c : s.toCharArray()) {
+				if (!st.isEmpty() && st.peek() == c) {
+					st.pop();
+				} else
+					st.push(c);
+			}
 
-            Stack<Character> st = new Stack<>();
-            for (char c : s.toCharArray()) {
-                if (!st.isEmpty() && st.peek() == c)
-                    st.pop();
-                else
-                    st.push(c);
-            }
+			if (st.isEmpty())
+				ans++;
+		}
 
-            if (st.isEmpty())
-                ans++;
-        }
-
-        bw.write(ans + "");
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.write(ans + "");
+		bw.flush();
+	}
 }
