a9f10a14b6386eb77f94d2d6ec551ada4e590124
Ward <rlawlsaud2149@gmail.com>
2026-03-20 12:21:32 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 120 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -2,33 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			Map<String, Integer> map = new HashMap<>();
+			for (int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				st.nextToken();
+				String s = st.nextToken();
+				map.put(s, map.getOrDefault(s, 0) + 1);
+			}
 
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
+			int ans = 1;
+			for (int i : map.values()) {
+				ans *= i + 1;
+			}
 
-            Map<String, Integer> map = new HashMap<>();
-            for (int i = 0; i < n; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                st.nextToken();
+			bw.write((ans - 1) + "\n");
+		}
 
-                String type = st.nextToken();
-                map.put(type, map.getOrDefault(type, 0) + 1);
-            }
-
-            int ans = 1;
-            for (int i : map.values())
-                ans *= i + 1;
-
-            bw.write((ans - 1) + "\n");
-
-        }
-
-
-        bw.flush();
-    }
+		bw.flush();
+	}
 }
