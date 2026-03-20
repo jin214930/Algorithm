7f29d8a832ad35829c1fd71ae2708298fd1db7e7
Ward <rlawlsaud2149@gmail.com>
2026-03-20 11:53:43 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 100 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -2,29 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        String p = br.readLine();
-        String pre = p.substring(0, p.indexOf('*'));
-        String suf = p.substring(p.indexOf('*') + 1);
+		int n = Integer.parseInt(br.readLine());
 
-        while (n-- > 0) {
-            String s = br.readLine();
+		String pattern = br.readLine();
 
-            if (pre.length() + suf.length() > s.length()) {
-                bw.write("NE\n");
-                continue;
-            }
+		int idx = pattern.indexOf('*');
+		String pre = pattern.substring(0, idx);
+		String suf = pattern.substring(idx + 1);
 
-            if (s.startsWith(pre) && s.endsWith(suf))
-                bw.write("DA\n");
-            else
-                bw.write("NE\n");
-        }
+		while (n-- > 0) {
+			String s = br.readLine();
+			if (s.length() < pre.length() + suf.length()) {
+				bw.write("NE\n");
+				continue;
+			}
 
-        bw.flush();
-    }
+			if (s.startsWith(pre) && s.endsWith(suf))
+				bw.write("DA\n");
+			else
+				bw.write("NE\n");
+		}
+
+		bw.flush();
+	}
 }
