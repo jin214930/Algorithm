10ea30acfc328ae4a485670c8f15c7fc5ef98038
Ward <rlawlsaud2149@gmail.com>
2026-03-21 11:47:08 +0900

    [Silver I] Title: 쿼드트리, Time: 124 ms, Memory: 15940 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -2,44 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static char[][] a;
+	static char[][] a;
 
-    static String go(int y, int x, int n) {
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        char tmp = a[y][x];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (tmp != a[y + i][x + j]) {
-                    sb.append("(");
-                    sb.append(go(y, x, n / 2));
-                    sb.append(go(y, x + n / 2, n / 2));
-                    sb.append(go(y + n / 2, x, n / 2));
-                    sb.append(go(y + n / 2, x + n / 2, n / 2));
-                    sb.append(")");
-                    return sb.toString();
-                }
-            }
-        }
+		int n = Integer.parseInt(br.readLine());
+		a = new char[n][n];
+		for (int i = 0; i < n; i++) {
+			a[i] = br.readLine().toCharArray();
+		}
 
-        sb.append(tmp);
-        return sb.toString();
-    }
+		bw.write(go(0, 0, n));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.flush();
+	}
 
-        int n = Integer.parseInt(br.readLine());
+	static String go(int y, int x, int n) {
+		char tmp = a[y][x];
+		for (int i = y; i < y + n; i++) {
+			for (int j = x; j < x + n; j++) {
+				if (tmp != a[i][j]) {
+					String ret = "(";
+					ret += go(y, x, n / 2);
+					ret += go(y, x + n / 2, n / 2);
+					ret += go(y + n / 2, x, n / 2);
+					ret += go(y + n / 2, x + n / 2, n / 2);
+					ret += ")";
+					return ret;
+				}
+			}
+		}
 
-        a = new char[n][n];
-        for (int i = 0; i < n; i++)
-            a[i] = br.readLine().toCharArray();
-
-        bw.write(go(0, 0, n));
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		return String.valueOf(tmp);
+	}
 }
