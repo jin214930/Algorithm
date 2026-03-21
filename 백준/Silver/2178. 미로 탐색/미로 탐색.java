0a34017df655ca0b1c318f4411d7e9426ad9602e
Ward <rlawlsaud2149@gmail.com>
2026-03-21 10:49:41 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14704 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,49 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int y, x;
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
 
-        Node(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		char[][] a = new char[n][m];
+		for (int i = 0; i < n; i++) {
+			a[i] = br.readLine().toCharArray();
+		}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int[][] visited = new int[n][m];
+		Queue<Node> q = new LinkedList<>();
+		q.add(new Node(0, 0));
+		visited[0][0] = 1;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+		while (!q.isEmpty()) {
+			Node node = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int ny = node.y + dy[i];
+				int nx = node.x + dx[i];
+				if (ny < 0 || nx < 0 || ny >= n || nx >= m)
+					continue;
+				if (a[ny][nx] == '0' || visited[ny][nx] != 0)
+					continue;
+				q.add(new Node(ny, nx));
+				visited[ny][nx] = visited[node.y][node.x] + 1;
+			}
+		}
 
-        char[][] a = new char[n][m];
-        for (int i = 0; i < n; i++)
-            a[i] = br.readLine().toCharArray();
+		bw.write(visited[n - 1][m - 1] + "");
 
-        int[][] visited = new int[n][m];
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0, 0));
-        visited[0][0] = 1;
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int ny = node.y + dy[i];
-                int nx = node.x + dx[i];
-                if (ny < 0 || nx < 0 || ny >= n || nx >= m || a[ny][nx] == '0' || visited[ny][nx] != 0) continue;
-                q.add(new Node(ny, nx));
-                visited[ny][nx] = visited[node.y][node.x] + 1;
-            }
-        }
+		bw.flush();
+	}
 
-        bw.write(visited[n - 1][m - 1] + "");
+	static class Node {
+		int y, x;
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		public Node(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+	}
 }
