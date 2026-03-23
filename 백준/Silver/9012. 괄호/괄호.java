bdb36a616f9bd4045c939cb2658d603931a4bf4e
Ward <rlawlsaud2149@gmail.com>
2026-03-23 11:10:13 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,28 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			String s = br.readLine();
+			Deque<Character> dq = new ArrayDeque<>();
+			for (char c : s.toCharArray()) {
+				if (c == '(') {
+					dq.add(c);
+				} else if (!dq.isEmpty() && dq.peek() == '(') {
+					dq.poll();
+				} else {
+					dq.add(c);
+				}
+			}
 
-        while (t-- > 0) {
-            String s = br.readLine();
+			bw.write((dq.isEmpty() ? "YES\n" : "NO\n"));
+		}
 
-            Stack<Character> st = new Stack<>();
-            for (char c : s.toCharArray()) {
-                if (!st.isEmpty() && st.peek() == '(' && c == ')')
-                    st.pop();
-                else
-                    st.push(c);
-            }
-
-            bw.write(st.isEmpty() ? "YES\n" : "NO\n");
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.flush();
+	}
 }
