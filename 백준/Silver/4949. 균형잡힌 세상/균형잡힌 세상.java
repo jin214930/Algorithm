fd1c8d8268c4cd78c2705a325742ae6bfc89c4b6
Ward <rlawlsaud2149@gmail.com>
2026-03-23 11:39:56 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 192 ms, Memory: 20116 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -2,38 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while (true) {
-            String s = br.readLine();
+		while (true) {
+			String s = br.readLine();
+			if (s.equals("."))
+				break;
 
-            if (s.equals("."))
-                break;
+			Stack<Character> st = new Stack<>();
+			for (char c : s.toCharArray()) {
+				if (c == '(' || c == '[') {
+					st.add(c);
+				} else if (c == ')') {
+					if (!st.isEmpty() && st.peek() == '(') {
+						st.pop();
+					} else {
+						st.add(c);
+					}
+				} else if (c == ']') {
+					if (!st.isEmpty() && st.peek() == '[') {
+						st.pop();
+					} else {
+						st.add(c);
+					}
+				}
+			}
 
-            Stack<Character> st = new Stack<>();
-            for (char c : s.toCharArray()) {
-                if (c == '(' || c == '[')
-                    st.push(c);
-                else if (c == ')') {
-                    if (!st.isEmpty() && st.peek() == '(')
-                        st.pop();
-                    else
-                        st.push(c);
-                } else if (c == ']') {
-                    if (!st.isEmpty() && st.peek() == '[')
-                        st.pop();
-                    else
-                        st.push(c);
-                }
-            }
+			bw.write((st.isEmpty() ? "yes\n" : "no\n"));
+		}
 
-            bw.write(st.isEmpty() ? "yes\n" : "no\n");
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.flush();
+	}
 }
