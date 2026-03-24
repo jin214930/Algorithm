bfa3babd1d1ff064571669ca4653bb6417b8aa6e
Ward <rlawlsaud2149@gmail.com>
2026-03-24 13:42:54 +0900

    [Gold IV] Title: 오큰수, Time: 936 ms, Memory: 151648 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,30 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++)
-            a[i] = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(br.readLine());
+		int[] a = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
 
-        Stack<Integer> stk = new Stack<>();
-        int[] ans = new int[n];
-        Arrays.fill(ans, -1);
-        for (int i = 0; i < n; i++) {
-            while (!stk.isEmpty() && a[stk.peek()] < a[i])
-                ans[stk.pop()] = a[i];
-            stk.push(i);
-        }
+		int[] ans = new int[n];
+		Arrays.fill(ans, -1);
 
-        for (int i : ans)
-            bw.write(i + " ");
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < n; i++) {
+			while (!stack.isEmpty() && a[stack.peek()] < a[i]) {
+				ans[stack.pop()] = a[i];
+			}
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+			stack.push(i);
+		}
+
+		for (int i : ans) {
+			bw.write(i + " ");
+		}
+
+		bw.flush();
+
+	}
 }
