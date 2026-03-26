9b4a79d2facecae217708a914811759d096f2cd2
Ward <rlawlsaud2149@gmail.com>
2026-03-27 02:20:23 +0900

    [Gold IV] Title: 알파벳, Time: 1076 ms, Memory: 16180 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int r, c, ans;
+	static char[][] a;
+	static boolean[][] visited;
+	static boolean[] alpha = new boolean[26];
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		a = new char[r][c];
+		for (int i = 0; i < r; i++) {
+			a[i] = br.readLine().toCharArray();
+		}
+
+		visited = new boolean[r][c];
+		alpha[a[0][0] - 'A'] = true;
+		visited[0][0] = true;
+		dfs(0, 0, 1);
+
+		bw.write(ans + "");
+
+		bw.flush();
+	}
+
+	static void dfs(int y, int x, int d) {
+		ans = Math.max(ans, d);
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= r || nx >= c)
+				continue;
+			if (visited[ny][nx] || alpha[a[ny][nx] - 'A'])
+				continue;
+			alpha[a[ny][nx] - 'A'] = true;
+			visited[ny][nx] = true;
+			dfs(ny, nx, d + 1);
+			alpha[a[ny][nx] - 'A'] = false;
+			visited[ny][nx] = false;
+		}
+	}
+
+}
