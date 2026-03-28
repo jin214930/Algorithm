f9b59afea239faa742f0cac20e763e31da4cfbf2
Ward <rlawlsaud2149@gmail.com>
2026-03-28 14:46:15 +0900

    [Gold III] Title: 불!, Time: 516 ms, Memory: 80256 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -2,83 +2,84 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int y, x;
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
 
-        Node(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Queue<Node> qP = new LinkedList<>();
+		int[][] visited = new int[r][c];
 
+		Queue<Node> qF = new LinkedList<>();
+		int[][] fire = new int[r][c];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+		char[][] a = new char[r][c];
+		for (int i = 0; i < r; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < c; j++) {
+				a[i][j] = s.charAt(j);
+				fire[i][j] = Integer.MAX_VALUE;
+				if (a[i][j] == 'J') {
+					qP.add(new Node(i, j));
+					visited[i][j] = 1;
+				} else if (a[i][j] == 'F') {
+					qF.add(new Node(i, j));
+					fire[i][j] = 1;
+				}
+			}
+		}
 
-        Queue<Node> qF = new LinkedList<>();
-        Queue<Node> qP = new LinkedList<>();
-        int[][] fire = new int[r][c];
-        int[][] visited = new int[r][c];
-        char[][] a = new char[r][c];
+		while (!qF.isEmpty()) {
+			Node node = qF.poll();
+			for (int i = 0; i < 4; i++) {
+				int ny = node.y + dy[i];
+				int nx = node.x + dx[i];
+				if (ny < 0 || nx < 0 || ny >= r || nx >= c)
+					continue;
+				if (a[ny][nx] == '#' || fire[ny][nx] != Integer.MAX_VALUE)
+					continue;
+				qF.add(new Node(ny, nx));
+				fire[ny][nx] = fire[node.y][node.x] + 1;
+			}
+		}
 
-        for (int i = 0; i < r; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < c; j++) {
-                a[i][j] = s.charAt(j);
-                if (a[i][j] == 'J') {
-                    qP.add(new Node(i, j));
-                    visited[i][j] = 1;
-                } else if (a[i][j] == 'F') {
-                    qF.add(new Node(i, j));
-                    fire[i][j] = 1;
-                }
-            }
-        }
+		while (!qP.isEmpty()) {
+			Node node = qP.poll();
+			if (node.y == 0 || node.x == 0 || node.y == r - 1 || node.x == c - 1) {
+				bw.write(visited[node.y][node.x] + "");
+				bw.flush();
+				return;
+			}
 
-        while (!qF.isEmpty()) {
-            Node node = qF.poll();
-            for (int i = 0; i < 4; i++) {
-                int ny = node.y + dy[i];
-                int nx = node.x + dx[i];
-                if (ny < 0 || nx < 0 || ny >= r || nx >= c || fire[ny][nx] != 0 || a[ny][nx] == '#') continue;
-                qF.add(new Node(ny, nx));
-                fire[ny][nx] = fire[node.y][node.x] + 1;
-            }
-        }
+			for (int i = 0; i < 4; i++) {
+				int ny = node.y + dy[i];
+				int nx = node.x + dx[i];
+				if (ny < 0 || nx < 0 || ny >= r || nx >= c)
+					continue;
+				if (a[ny][nx] == '#' || visited[ny][nx] != 0 || fire[ny][nx] <= visited[node.y][node.x] + 1)
+					continue;
+				qP.add(new Node(ny, nx));
+				visited[ny][nx] = visited[node.y][node.x] + 1;
+			}
+		}
 
-        int ans = -1;
-        while (!qP.isEmpty()) {
-            Node node = qP.poll();
-            if (node.y == 0 || node.y == r - 1 || node.x == 0 || node.x == c - 1) {
-                ans = visited[node.y][node.x];
-                break;
-            }
-            for (int i = 0; i < 4; i++) {
-                int ny = node.y + dy[i];
-                int nx = node.x + dx[i];
-                if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] != 0 || a[ny][nx] == '#') continue;
-                if (fire[ny][nx] != 0 && visited[node.y][node.x] + 1 >= fire[ny][nx]) continue;
-                qP.add(new Node(ny, nx));
-                visited[ny][nx] = visited[node.y][node.x] + 1;
-            }
-        }
+		bw.write("IMPOSSIBLE");
 
-        if (ans == -1)
-            bw.write("IMPOSSIBLE\n");
-        else
-            bw.write(ans + "\n");
+		bw.flush();
+	}
 
+	static class Node {
+		int y, x;
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		Node(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+	}
 }
