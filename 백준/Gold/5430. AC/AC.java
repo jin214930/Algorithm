28748f0821e0f56e3ef84b8326daba33ce3f0754
Ward <rlawlsaud2149@gmail.com>
2026-03-29 18:35:35 +0900

    [Gold V] Title: AC, Time: 656 ms, Memory: 92512 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -2,55 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String p = br.readLine();
-            int n = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine(), "[,]");
-            Deque<String> dq = new ArrayDeque<>();
-            for (int i = 0; i < n; i++)
-                dq.add(st.nextToken());
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			String p = br.readLine();
+			int n = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine(), "[,]");
+			Deque<Integer> dq = new ArrayDeque<>();
+			boolean isReverse = false;
+			for (int i = 0; i < n; i++) {
+				dq.addLast(Integer.parseInt(st.nextToken()));
+			}
 
-            boolean isReverse = false;
-            boolean isError = false;
-            for (char c : p.toCharArray()) {
-                if (c == 'R')
-                    isReverse = !isReverse;
-                else {
-                    if (dq.isEmpty()) {
-                        isError = true;
-                        break;
-                    }
+			boolean isError = false;
+			for (char c : p.toCharArray()) {
+				if (c == 'R') {
+					isReverse = !isReverse;
+				} else if (dq.isEmpty()) {
+					isError = true;
+					break;
+				} else if (isReverse) {
+					dq.pollLast();
+				} else {
+					dq.pollFirst();
+				}
+			}
 
-                    if (isReverse)
-                        dq.pollLast();
-                    else
-                        dq.poll();
-                }
-            }
+			if (isError) {
+				bw.write("error\n");
+				continue;
+			}
 
-            if (isError) {
-                bw.write("error\n");
-                bw.flush();
-                continue;
-            }
+			StringBuilder sb = new StringBuilder();
+			sb.append("[");
+			if (isReverse) {
+				while (!dq.isEmpty()) {
+					sb.append(dq.pollLast());
+					if (!dq.isEmpty()) {
+						sb.append(",");
+					}
+				}
+			} else {
+				while (!dq.isEmpty()) {
+					sb.append(dq.pollFirst());
+					if (!dq.isEmpty()) {
+						sb.append(",");
+					}
+				}
+			}
 
-            bw.write("[");
-            while (!dq.isEmpty()) {
-                if (isReverse)
-                    bw.write(dq.pollLast());
-                else
-                    bw.write(dq.poll());
+			sb.append("]\n");
+			bw.write(sb.toString());
 
-                if (!dq.isEmpty())
-                    bw.write(",");
-            }
-            bw.write("]\n");
-            bw.flush();
-        }
-    }
+		}
+
+		bw.flush();
+	}
 }
