0fde980329bd5d2acc1e64b0b8aa98d3169ca7ed
Ward <rlawlsaud2149@gmail.com>
2026-03-31 00:41:37 +0900

    [Silver II] Title: 연속합, Time: 260 ms, Memory: 23416 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,28 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++)
-            a[i] = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(br.readLine());
+		int[] a = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
 
-        int[] d = new int[n];
-        d[0] = a[0];
-        int ans = d[0];
-        for (int i = 1; i < n; i++) {
-            d[i] = Math.max(a[i], d[i - 1] + a[i]);
-            ans = Math.max(ans, d[i]);
-        }
+		int[] d = new int[n];
+		Arrays.fill(d, Integer.MIN_VALUE);
+		int ans = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			d[i] = a[i];
+			if (i > 0) {
+				d[i] = Math.max(d[i - 1] + a[i], d[i]);
+			}
 
-        bw.write(ans + "");
+			ans = Math.max(ans, d[i]);
+		}
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.write(ans + "");
+
+		bw.flush();
+	}
 }
