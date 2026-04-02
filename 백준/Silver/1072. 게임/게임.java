851d88c7a36c25139a31239a2ce3da094b473bbd
Ward <rlawlsaud2149@gmail.com>
2026-04-02 13:26:17 +0900

    [Silver III] Title: 게임, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -1,24 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		int z = (int) (y * 100L / x);
-		int ans = -1;
-		int s = 1, e = 1000000000;
+		long x = Integer.parseInt(st.nextToken());
+		long y = Integer.parseInt(st.nextToken());
+		long z = 100 * y / x;
+
+		if (z >= 99) {
+			bw.write("-1");
+			bw.flush();
+			return;
+		}
+
+		long s = 1;
+		long e = 1000000000;
+		long ans = -1;
 		while (s <= e) {
-			int m = (s + e) / 2;
-			int tmp = (int) ((y + m) * 100L / (x + m));
-			if (z < tmp) {
+			long m = (s + e) / 2;
+			if (100 * (y + m) / (x + m) > z) {
 				ans = m;
 				e = m - 1;
-			} else
+			} else {
 				s = m + 1;
+			}
 		}
-		System.out.println(ans);
+
+		bw.write(ans + "");
+
+		bw.flush();
 	}
 }
