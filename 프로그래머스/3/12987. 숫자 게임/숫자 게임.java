2f2f893014ac497d6693e855b0c66e2b27d66e77
Ward <rlawlsaud2149@gmail.com>
2026-04-03 22:47:49 +0900

    [level 3] Title: 숫자 게임, Time: 138.94 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int ans = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int idx = B.length - 1;
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (A[i] < B[idx]) {
+                ans++;
+                idx--;
+            }
+        }
+        
+        return ans;
+    }
+}
