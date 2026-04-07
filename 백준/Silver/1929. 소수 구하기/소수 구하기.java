f37013800c3ade964383a29ed1dae15fed2d8e8b
Ward <rlawlsaud2149@gmail.com>
2026-04-07 23:58:43 +0900

    [Silver III] Title: 소수 구하기, Time: 248 ms, Memory: 21708 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,36 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int gcd(int a, int b) {
-        if (b == 0)
-            return a;
-        return gcd(b, a % b);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		boolean[] p = new boolean[1000001];
+		p[1] = true;
+		for (int i = 2; i <= 1000000; i++) {
+			if (p[i])
+				continue;
+			for (int j = 2 * i; j <= 1000000; j += i) {
+				p[j] = true;
+			}
+		}
 
-        boolean[] p = new boolean[1000001];
-        p[1] = true;
-        for (int i = 2; i <= 1000000; i++) {
-            if (!p[i]) {
-                for (int j = i * 2; j <= 1000000; j += i)
-                    p[j] = true;
-            }
-        }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+		for (int i = n; i <= m; i++) {
+			if (!p[i]) {
+				bw.write(i + "\n");
+			}
+		}
 
-        for (int i = a; i <= b; i++) {
-            if (!p[i])
-                bw.write(i + "\n");
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.flush();
+	}
 }
