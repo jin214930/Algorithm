f6cd55bc97c9bfeec58031594d396128b19f6902
Ward <rlawlsaud2149@gmail.com>
2026-04-22 19:45:21 +0900

    [Gold III] Title: 오등큰수, Time: 948 ms, Memory: 147252 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -2,36 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] a = new int[n];
-        int[] ans = new int[n];
-        Map<Integer, Integer> map = new HashMap<>();
+		int n = Integer.parseInt(br.readLine());
+		int[] a = new int[n];
+		int[] cnt = new int[1000001];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
-            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
-            ans[i] = -1;
-        }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+			cnt[a[i]]++;
+		}
 
-        Deque<Integer> stk = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            while (!stk.isEmpty() && map.get(a[stk.peek()]) < map.get(a[i])) {
-                ans[stk.pop()] = a[i];
-            }
+		int[] ans = new int[n];
+		Arrays.fill(ans, -1);
 
-            stk.push(i);
-        }
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < n; i++) {
+			while (!stack.isEmpty() && cnt[a[stack.peek()]] < cnt[a[i]]) {
+				ans[stack.pop()] = a[i];
+			}
+			stack.push(i);
+		}
 
-        for (int i : ans)
-            bw.write(i + " ");
+		for (int i = 0; i < n; i++) {
+			bw.write(ans[i] + " ");
+		}
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.flush();
+	}
 }
