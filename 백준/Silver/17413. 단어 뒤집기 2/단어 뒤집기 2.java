0d8b0d274856dcd1d28efb9fd99c89716d62a18e
Ward <rlawlsaud2149@gmail.com>
2026-04-22 19:34:37 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 184 ms, Memory: 17288 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -2,33 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Deque<Character> st = new ArrayDeque<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String s = br.readLine() + " ";
+		String s = br.readLine();
 
-        boolean flag = false;
-        for (char c : s.toCharArray()) {
-            if (flag) {
-                bw.write(c);
-                if (c == '>')
-                    flag = false;
-            } else {
-                if (c == ' ' || c == '<') {
-                    while (!st.isEmpty())
-                        bw.write(st.pop());
-                    bw.write(c);
-                    if (c == '<')
-                        flag = true;
-                } else
-                    st.push(c);
-            }
-        }
+		Stack<Character> st = new Stack<>();
+		boolean flag = false;
+		for (char c : s.toCharArray()) {
+			if (flag) {
+				if (c == '>')
+					flag = false;
+				bw.write(c);
+				continue;
+			}
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+			if (c == '<' || c == ' ') {
+				while (!st.isEmpty()) {
+					bw.write(st.pop());
+				}
+				if (c == '<') {
+					flag = true;
+				}
+				bw.write(c);
+				continue;
+			}
+
+			st.push(c);
+		}
+
+		while (!st.isEmpty()) {
+			bw.write(st.pop());
+		}
+
+		bw.flush();
+	}
 }
