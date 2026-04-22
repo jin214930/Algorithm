d12dfd48cb30ab0e41372e4c51942c1f0bbb04fd
Ward <rlawlsaud2149@gmail.com>
2026-04-22 22:16:17 +0900

    [Gold III] Title: 사다리 조작, Time: 448 ms, Memory: 18068 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static boolean[][] map;
+	static int n, m, h;
+	static int ans = 4;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+
+		map = new boolean[h + 1][n + 1];
+		while (m-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			map[a][b] = true;
+		}
+
+		go(0, 1, 1);
+
+		bw.write((ans == 4 ? -1 : ans) + "");
+		bw.flush();
+	}
+
+	static void go(int idx, int r, int c) {
+		if (idx >= ans) {
+			return;
+		}
+
+		if (check()) {
+			ans = idx;
+			return;
+		}
+
+		if (idx == 3) {
+			return;
+		}
+
+		for (int i = r; i <= h; i++) {
+			int start = (i == r) ? c : 1;
+			for (int j = start; j < n; j++) {
+				if (!map[i][j] && !map[i][j - 1] && !map[i][j + 1]) {
+					map[i][j] = true;
+					go(idx + 1, i, j + 2);
+					map[i][j] = false;
+				}
+			}
+		}
+	}
+
+	static boolean check() {
+		for (int i = 1; i <= n; i++) {
+			int tmp = i;
+			for (int j = 1; j <= h; j++) {
+				if (map[j][tmp]) {
+					tmp++;
+				} else if (tmp > 1 && map[j][tmp - 1]) {
+					tmp--;
+				}
+			}
+			if (tmp != i)
+				return false;
+		}
+		return true;
+	}
+}
