10a51a8a81a143958050951e2ac0c4100e09764a
Ward <rlawlsaud2149@gmail.com>
2026-04-23 23:52:27 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,8 +1,11 @@
 class Solution {
-    public int[] solution(int brown, int yellow) {        
+    public int[] solution(int brown, int yellow) {
         for (int i = 1; i * i <= yellow; i++) {
-            if (yellow % i == 0 && (i + yellow / i) * 2 + 4 == brown) 
-                    return new int[]{yellow / i + 2, i + 2};
+            if (yellow % i != 0) continue;
+            int j = yellow / i;
+            if (4 + 2 * (i + j) == brown) {
+                return new int[] {j + 2, i + 2};
+            }
         }
         
         return null;
