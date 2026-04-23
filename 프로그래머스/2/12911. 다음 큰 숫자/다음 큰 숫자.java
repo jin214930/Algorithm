43e93397b80349323e80977052b27a7858cedbb9
Ward <rlawlsaud2149@gmail.com>
2026-04-23 21:19:17 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,11 +1,28 @@
 class Solution {
     public int solution(int n) {
-        int cnt = Integer.bitCount(n);
+        int cnt = 0;
+        for (char c : Integer.toBinaryString(n).toCharArray()) {
+            if (c == '1') {
+                cnt++;
+            }
+        }
         
-        while (true) {
-            n++;
-            if (Integer.bitCount(n) == cnt)
-                return n;  
-        }       
+        int ans = n + 1;
+        while(true) {
+            int cnt2 = 0;
+            for (char c : Integer.toBinaryString(ans).toCharArray()) {
+                if (c == '1') {
+                    cnt2++;
+                }
+            }
+            
+            if (cnt == cnt2) {
+                break;
+            }
+            
+            ans++;
+        }
+        
+        return ans;
     }
 }
