1d4ef1a7e88473457bed52c6cd90e1ef0c30e906
Ward <rlawlsaud2149@gmail.com>
2026-04-25 18:08:23 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,18 +1,16 @@
 class Solution {
     public int solution(int n, int a, int b) {
-        if (a > b) {
-            int tmp = a;
-            a = b;
-            b = tmp;
-        }
-        
-        int ans = 1;
-        while (true) {
-            if (a % 2 == 1 && b == a + 1)
-                return ans;
-            a = a / 2 + (a % 2 == 0 ? 0 : 1);
-            b = b / 2 + (b % 2 == 0 ? 0 : 1);
+        int ans = 0;
+        a--;
+        b--;
+        while(true) {
             ans++;
+            if (a / 2 == b / 2) {
+                return ans;
+            }
+            
+            a /= 2;
+            b /= 2;
         }
     }
 }
