5b2332520eb355e745883dd7f3857bb95e2f9864
Ward <rlawlsaud2149@gmail.com>
2026-04-26 23:27:40 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -2,38 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int gcd(int a, int b) {
-        if (b == 0)
-            return a;
-        return gcd(b, a % b);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        boolean[] p = new boolean[1001];
-        p[1] = true;
-        for (int i = 2; i <= 1000; i++) {
-            if (!p[i]) {
-                for (int j = i * 2; j <= 1000; j += i)
-                    p[j] = true;
-            }
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
         int ans = 0;
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            if (!p[x])
+        for (int x : a) {
+            boolean isPrime = true;
+            for (int i = 2; i * i <= x; i++) {
+                if (x % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if (x != 1 && isPrime) {
                 ans++;
+            }
         }
 
         bw.write(ans + "");
-
         bw.flush();
-        bw.close();
-        br.close();
     }
 }
