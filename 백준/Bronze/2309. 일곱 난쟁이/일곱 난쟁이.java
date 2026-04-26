a0b61a2b68285b7772522f519d176c7b7e479cc4
Ward <rlawlsaud2149@gmail.com>
2026-04-27 01:56:04 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 116 ms, Memory: 15944 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,44 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] a = new int[9];
-	static List<Integer> ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] a = new int[9];
 
-		for (int i = 0; i < 9; i++) {
-			a[i] = Integer.parseInt(br.readLine());
-		}
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+            sum += a[i];
+        }
 
-		Arrays.sort(a);
+        Arrays.sort(a);
 
-		go(0, new ArrayList<>());
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (sum - a[i] - a[j] == 100) {
+                    for (int k = 0; k < a.length; k++) {
+                        if (k != i && k != j) {
+                            bw.write(a[k] + "\n");
+                        }
+                    }
 
-		for (int i : ans) {
-			bw.write(i + "\n");
-		}
-		bw.flush();
-	}
-
-	static void go(int idx, List<Integer> list) {
-		if (list.size() == 7) {
-			int sum = 0;
-			for (int i = 0; i < 7; i++) {
-				sum += list.get(i);
-			}
-
-			if (sum == 100) {
-				ans = List.copyOf(list);
-			}
-			return;
-		}
-
-		for (int i = idx; i < 9; i++) {
-			list.add(a[i]);
-			go(i + 1, list);
-			list.remove(list.size() - 1);
-		}
-	}
+                    bw.flush();
+                    return;
+                }
+            }
+        }
+    }
 }
