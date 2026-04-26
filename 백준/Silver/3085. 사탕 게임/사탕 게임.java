975e2c8a65de214035b827eabc11db87808e3155
Ward <rlawlsaud2149@gmail.com>
2026-04-27 02:09:49 +0900

    [Silver II] Title: 사탕 게임, Time: 168 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -2,74 +2,87 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static char[][] a;
+	static int n, ans;
+	static char[][] a;
 
-    static int count() {
-        int ret = 0;
-        for (int i = 0; i < n; i++) {
-            int tmp = 1;
-            for (int j = 0; j < n - 1; j++) {
-                if (a[i][j] != a[i][j + 1])
-                    tmp = 0;
-                tmp++;
-                ret = Math.max(ret, tmp);
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int j = 0; j < n; j++) {
-            int tmp = 1;
-            for (int i = 0; i < n - 1; i++) {
-                if (a[i][j] != a[i + 1][j])
-                    tmp = 0;
-                tmp++;
-                ret = Math.max(ret, tmp);
-            }
-        }
+		n = Integer.parseInt(br.readLine());
+		a = new char[n][n];
+		for (int i = 0; i < n; i++) {
+			a[i] = br.readLine().toCharArray();
+		}
 
-        return ret;
-    }
+		go();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i != n - 1) {
+					char tmp = a[i][j];
+					a[i][j] = a[i + 1][j];
+					a[i + 1][j] = tmp;
+					go();
+					tmp = a[i][j];
+					a[i][j] = a[i + 1][j];
+					a[i + 1][j] = tmp;
+				}
+				if (j != n - 1) {
+					char tmp = a[i][j];
+					a[i][j] = a[i][j + 1];
+					a[i][j + 1] = tmp;
+					go();
+					tmp = a[i][j];
+					a[i][j] = a[i][j + 1];
+					a[i][j + 1] = tmp;
+				}
+			}
+		}
 
-    static void swap(int y1, int x1, int y2, int x2) {
-        char tmp = a[y1][x1];
-        a[y1][x1] = a[y2][x2];
-        a[y2][x2] = tmp;
-    }
+		bw.write(ans + "");
+		bw.flush();
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static void go() {
+		int ret = 0;
+		for (int i = 0; i < n; i++) {
+			char tmp = '\0';
+			int cnt = 0;
+			for (int j = 0; j < n; j++) {
+				if (tmp == a[i][j]) {
+					cnt++;
+				} else {
+					ret = Math.max(ret, cnt);
+					cnt = 1;
+					tmp = a[i][j];
+				}
 
-        n = Integer.parseInt(br.readLine());
-        a = new char[n][n];
-        for (int i = 0; i < n; i++)
-            a[i] = br.readLine().toCharArray();
+				if (j == n - 1) {
+					tmp = '\0';
+					ret = Math.max(ret, cnt);
+				}
+			}
+		}
 
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                if (a[i][j] != a[i][j + 1]) {
-                    swap(i, j, i, j + 1);
-                    ans = Math.max(ans, count());
-                    swap(i, j, i, j + 1);
-                }
-            }
-        }
+		for (int i = 0; i < n; i++) {
+			char tmp = '\0';
+			int cnt = 0;
+			for (int j = 0; j < n; j++) {
+				if (tmp == a[j][i]) {
+					cnt++;
+				} else {
+					ret = Math.max(ret, cnt);
+					cnt = 1;
+					tmp = a[j][i];
+				}
 
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < n - 1; i++) {
-                if (a[i][j] != a[i + 1][j]) {
-                    swap(i, j, i + 1, j);
-                    ans = Math.max(ans, count());
-                    swap(i, j, i + 1, j);
-                }
-            }
-        }
+				if (j == n - 1) {
+					tmp = '\0';
+					ret = Math.max(ret, cnt);
+				}
+			}
+		}
 
-        bw.write(ans + "");
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		ans = Math.max(ans, ret);
+	}
 }
