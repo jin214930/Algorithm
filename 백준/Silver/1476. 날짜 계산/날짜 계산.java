f2b41411e104cca6cd7334307898aa4a38814323
Ward <rlawlsaud2149@gmail.com>
2026-04-27 02:25:18 +0900

    [Silver V] Title: 날짜 계산, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.java
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -2,28 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int e = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        e--;
-        s--;
-        m--;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int e = Integer.parseInt(st.nextToken()) - 1;
+		int s = Integer.parseInt(st.nextToken()) - 1;
+		int m = Integer.parseInt(st.nextToken()) - 1;
 
-        int ans = 0;
+		for (int i = 0;; i++) {
+			if (i % 15 == e && i % 28 == s && i % 19 == m) {
+				bw.write((i + 1) + "");
+				break;
+			}
+		}
 
-        while (ans % 15 != e || ans % 28 != s || ans % 19 != m) {
-            ans++;
-        }
-
-        bw.write(ans + 1 + "");
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.flush();
+	}
 }
