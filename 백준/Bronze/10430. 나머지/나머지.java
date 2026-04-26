353121be9a77c439c56517eaed4917d7d6109504
Ward <rlawlsaud2149@gmail.com>
2026-04-26 22:22:38 +0900

    [Bronze V] Title: 나머지, Time: 116 ms, Memory: 15780 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -2,22 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        bw.write((a + b) % c + "\n");
-        bw.write(((a % c) + (b % c)) % c + "\n");
-        bw.write((a * b) % c + "\n");
-        bw.write(((a % c) * (b % c)) % c + "");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		bw.write((a + b) % c + "\n");
+		bw.write(((a % c) + (b % c)) % c + "\n");
+		bw.write((a * b) % c + "\n");
+		bw.write(((a % c) * (b % c)) % c + "\n");
+		bw.flush();
+	}
 }
-
