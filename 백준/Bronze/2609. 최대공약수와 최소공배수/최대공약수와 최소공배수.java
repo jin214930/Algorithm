2d1cb692dd1b3e353cfdec81787cebb4f8669550
Ward <rlawlsaud2149@gmail.com>
2026-04-26 23:16:38 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 128 ms, Memory: 16160 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,28 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static int gcd(int a, int b) {
-        if (b > a)
-            return gcd(b, a);
-        if (b == 0)
-            return a;
-        return gcd(b, a % b);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 
-        bw.write(gcd(a, b) + "\n");
-        bw.write(a * b / gcd(a, b) + "");
+		int gcd = gcd(a, b);
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.write(gcd + "\n" + (a * b / gcd));
+		bw.flush();
+	}
+
+	static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+
+		return gcd(b, a % b);
+	}
 }
-
