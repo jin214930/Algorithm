352bca789e1c746b9e1cce3332d6a001b98a0af9
Ward <rlawlsaud2149@gmail.com>
2026-04-27 11:52:59 +0900

    [Silver I] Title: 카잉 달력, Time: 648 ms, Memory: 42756 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.java
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -2,46 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int m = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			int y = Integer.parseInt(st.nextToken()) - 1;
 
-        int t = Integer.parseInt(br.readLine());
+			boolean flag = true;
+			for (int i = x; i < m * n; i += m) {
+				if (i % n == y) {
+					bw.write((i + 1) + "\n");
+					flag = false;
+					break;
+				}
+			}
 
-        while (t-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+			if (flag) {
+				bw.write("-1\n");
+			}
+		}
 
-            int m = Integer.parseInt(st.nextToken());
-            int n = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            x--;
-            y--;
-
-            int ans = 0;
-            boolean flag = true;
-            for (int i = x; i < m * n; i += m) {
-                if (i % n == y) {
-                    bw.write(i + 1 + "\n");
-                    flag = false;
-                    break;
-                }
-            }
-
-            if (flag)
-                bw.write("-1\n");
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.flush();
+	}
 }
