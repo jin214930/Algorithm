bd3d00666146a01c06c8250b2804e070be4f3f99
Ward <rlawlsaud2149@gmail.com>
2026-04-28 01:15:27 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -2,23 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int ans = 0;
-        int tmp = 9;
-        while (n > 0) {
-            ans += n;
-            n -= tmp;
-            tmp *= 10;
-        }
+		int n = Integer.parseInt(br.readLine());
+		long ans = 0;
+		int tmp = 0;
+		while (tmp < n) {
+			ans += n - tmp;
+			tmp = tmp * 10 + 9;
+		}
 
-        bw.write(ans + "");
+		bw.write(ans + "");
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		bw.flush();
+	}
 }
