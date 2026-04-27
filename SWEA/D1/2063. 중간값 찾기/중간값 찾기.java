2ae88818ac50eebeba7b796230d1deb0979283a2
Ward <rlawlsaud2149@gmail.com>
2026-04-27 18:49:17 +0900

    [D1] Title: 중간값 찾기, Time: 159 ms, Memory: 20,812 KB -BaekjoonHub

---
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+
+        bw.write(a[n / 2] + "");
+        bw.flush();
+    }
+}
