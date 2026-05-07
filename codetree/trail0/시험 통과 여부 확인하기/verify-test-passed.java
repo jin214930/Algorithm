19608db20d5e3390a676a680c114faa4caf53e62
김진명 <rlawlsaud2149@gmail.com>
2026-05-07 16:40:11 +0900

    [Passed] 시험 통과 여부 확인하기 (94ms, 9MB)

---
+++ codetree/trail0/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+
+		bw.write(n >= 80 ? "pass" : (80 - n) + " more score");
+
+		bw.flush();
+	}
+}
