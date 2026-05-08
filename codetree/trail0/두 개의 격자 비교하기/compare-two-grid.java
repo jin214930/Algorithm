a2f61d5728129bc2c35c3bec93ae431264fd8b17
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 12:27:57 +0900

    [Passed] 두 개의 격자 비교하기 (93ms, 9MB)

---
+++ codetree/trail0/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[][] a = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				a[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int[][] b = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				b[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				bw.write(a[i][j] == b[i][j] ? "0 " : "1 ");
+			}
+			bw.write("\n");
+		}
+
+		bw.flush();
+	}
+}
