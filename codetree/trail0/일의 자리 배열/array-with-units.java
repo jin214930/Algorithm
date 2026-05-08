c7b45d42a6059211a3dedb155de70c9dd2ffd20b
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 09:27:16 +0900

    [Passed] 일의 자리 배열 (92ms, 9MB)

---
+++ codetree/trail0/일의 자리 배열/array-with-units.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		bw.write(a + " ");
+		bw.write(b + " ");
+		for (int i = 0; i < 8; i++) {
+			int tmp = (a + b) % 10;
+			a = b;
+			b = tmp;
+			bw.write(tmp + " ");
+		}
+
+		bw.flush();
+	}
+}
