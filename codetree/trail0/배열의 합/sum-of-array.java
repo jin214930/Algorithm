1d18e33da5520b8ac0480f033651fbff179e8599
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 12:31:46 +0900

    [Passed] 배열의 합 (95ms, 11MB)

---
+++ codetree/trail0/배열의 합/sum-of-array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for (int i = 0; i < 4; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int sum = 0;
+			for (int j = 0; j < 4;j ++) {
+				sum += Integer.parseInt(st.nextToken());
+			}
+			bw.write(sum + "\n");
+		}
+
+		bw.flush();
+	}
+}
