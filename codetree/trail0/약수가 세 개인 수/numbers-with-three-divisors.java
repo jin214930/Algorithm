496814f9f57e176f4dd572a61c1ee4bf5a283c6c
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 12:17:15 +0900

    [Passed] 약수가 세 개인 수 (104ms, 9MB)

---
+++ codetree/trail0/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		int ans = 0;
+		for (int i = a; i <= b; i++) {
+			int cnt = 0;
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					cnt++;
+				}
+			}
+			if (cnt == 3) {
+				ans++;
+			}
+		}
+		
+		bw.write(ans + "\n");
+		bw.flush();
+	}
+}
