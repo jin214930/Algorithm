d3042cba0958629bb5d37b07b04e002de668db09
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 09:24:57 +0900

    [Passed] 제곱하여 출력하기 (95ms, 9MB)

---
+++ codetree/trail0/제곱하여 출력하기/print-square-of-elements.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i < n; i++) {
+			bw.write(a[i] * a[i] + " ");
+		}
+
+		bw.flush();
+	}
+}
