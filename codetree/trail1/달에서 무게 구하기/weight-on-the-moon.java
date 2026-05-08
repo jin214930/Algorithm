6f39e254da8611a2a7427fc770c30081f39a84ad
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 13:02:15 +0900

    [Passed] 달에서 무게 구하기 (96ms, 9MB)

---
+++ codetree/trail1/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("13 * 0.165000 = 2.145000");
+
+		bw.flush();
+	}
+}
