0018940bbf2200b959f8cd129f2b63455bf440b4
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 13:02:48 +0900

    [Passed] 길이 단위 변환하기 (90ms, 9MB)

---
+++ codetree/trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("9.2ft = 280.4cm\n"
+			+ "1.3mi = 209214.2cm");
+
+		bw.flush();
+	}
+}
