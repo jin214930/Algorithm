9b05308d5f6b047a106a7c1ce299f86a50f26fdd
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 12:55:02 +0900

    [Passed] 다양하게 출력 (89ms, 9MB)

---
+++ codetree/trail1/다양하게 출력/print-in-variety.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("Total days in Year\n"
+			+ "365\n"
+			+ "Circumference rate\n"
+			+ "3.1415926535\n");
+
+		bw.flush();
+	}
+}
