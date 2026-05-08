0e73f18b061f9041e70d5e516969f96aec6cadb0
김진명 <rlawlsaud2149@gmail.com>
2026-05-08 09:12:02 +0900

    [Passed] 점수 비교 (88ms, 9MB)

---
+++ codetree/trail0/점수 비교/score-comparison.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a1 = Integer.parseInt(st.nextToken());
+		int a2 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int b1 = Integer.parseInt(st.nextToken());
+		int b2 = Integer.parseInt(st.nextToken());
+		
+		if (a1 > b1 && a2 > b2) {
+			bw.write("1");
+		} else {
+			bw.write("0");
+		}
+
+		bw.flush();
+	}
+}
