f57eeb60bc48fd6e378df508ce7b946fd6f010a2
김진명 <rlawlsaud2149@gmail.com>
2026-05-10 01:39:24 +0900

    [Passed] 정수 입력받아 계산 (86ms, 9MB)

---
+++ codetree/trail1/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        bw.write(2 * n + 3 + "");
+
+        bw.flush();
+    }
+}
