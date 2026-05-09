83542c3f23c3b40f88d8c4e1e247fc101aadf0c7
김진명 <rlawlsaud2149@gmail.com>
2026-05-10 01:41:19 +0900

    [Passed] 실수 받아 그대로 출력 (103ms, 9MB)

---
+++ codetree/trail1/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        double n = Double.parseDouble(br.readLine());
+        bw.write(String.format("%.2f", n));
+
+        bw.flush();
+    }
+}
