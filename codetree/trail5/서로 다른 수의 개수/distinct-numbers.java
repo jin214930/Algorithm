2760c52ed62563b76aba74f490edcd398006c60e
김진명 <rlawlsaud2149@gmail.com>
2026-05-10 16:09:42 +0900

    [Passed] 서로 다른 수의 개수 (290ms, 22MB)

---
+++ codetree/trail5/서로 다른 수의 개수/distinct-numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Set<Integer> set = new HashSet<>();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        bw.write(set.size() + "");
+        
+        bw.flush();
+    }
+}
