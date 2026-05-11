d547e3c836fd1c93ba752151cec1e5e937315209
김진명 <rlawlsaud2149@gmail.com>
2026-05-11 20:30:49 +0900

    [Passed] 정수 세 개 입력받아 출력 (89ms, 9MB)

---
+++ codetree/trail1/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        
+        bw.write(a + " " + b + " " + c);
+        
+        bw.flush();
+    }
+}
