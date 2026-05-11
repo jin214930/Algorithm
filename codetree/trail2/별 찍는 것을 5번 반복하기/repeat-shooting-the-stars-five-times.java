091cecfef30a54575666ac29a622df85079ed536
김진명 <rlawlsaud2149@gmail.com>
2026-05-12 00:08:38 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (91ms, 9MB)

---
+++ codetree/trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        
+        bw.flush();
+    }
+    
+}
