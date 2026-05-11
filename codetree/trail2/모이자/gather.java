647e4db4a89b118a566fb618080c5a6e01fb4014
김진명 <rlawlsaud2149@gmail.com>
2026-05-11 21:42:47 +0900

    [Passed] 모이자 (98ms, 9MB)

---
+++ codetree/trail2/모이자/gather.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += Math.abs(a[j] * Math.abs(i - j));
+            }
+            ans = Math.min(ans, sum);
+        }
+
+        bw.write(ans + "");
+
+        bw.flush();
+    }
+}
