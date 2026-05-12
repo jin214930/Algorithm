d6bffc3328634f66878ed5299e0bf3427005c66b
김진명 <rlawlsaud2149@gmail.com>
2026-05-12 19:07:26 +0900

    [Passed] 정사각형의 넓이 (91ms, 9MB)

---
+++ codetree/trail1/정사각형의 넓이/area-of-a-rectangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        bw.write(n * n +"\n");
+        if (n < 5) {
+            bw.write("tiny");
+        }
+
+        bw.flush();
+    }
+}
