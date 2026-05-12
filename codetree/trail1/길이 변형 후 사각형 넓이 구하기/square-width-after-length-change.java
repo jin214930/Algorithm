61948d4dec9ede62844e596510957986cf7ba1c3
김진명 <rlawlsaud2149@gmail.com>
2026-05-12 18:51:54 +0900

    [Passed] 길이 변형 후 사각형 넓이 구하기 (90ms, 9MB)

---
+++ codetree/trail1/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        a += 8;
+        b *= 3;
+        bw.write(a + "\n");
+        bw.write(b + "\n");
+        bw.write(a * b + "\n");
+
+        bw.flush();
+    }
+}
