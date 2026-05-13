70bf03f8db35465495945d874a0e7c892ae0e4e5
김진명 <rlawlsaud2149@gmail.com>
2026-05-13 18:35:52 +0900

    [Passed] 등급 매기기 (95ms, 9MB)

---
+++ codetree/trail1/등급 매기기/ranking.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        if (n >= 90) {
+            bw.write("A");
+        } else if (n >= 80) {
+            bw.write("B");
+        } else if (n >= 70) {
+            bw.write("C");
+        } else if (n >= 60) {
+            bw.write("D");
+        } else {
+            bw.write("F");
+        }
+        
+        bw.flush();
+    }
+}
