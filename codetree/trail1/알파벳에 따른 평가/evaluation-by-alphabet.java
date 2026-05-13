1e538734f852952459b57f380ce1048f845b85b5
김진명 <rlawlsaud2149@gmail.com>
2026-05-13 18:37:00 +0900

    [Passed] 알파벳에 따른 평가 (90ms, 9MB)

---
+++ codetree/trail1/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String s = br.readLine();
+        
+        if (s.equals("S")) {
+            bw.write("Superior");
+        } else if (s.equals("A")) {
+            bw.write("Excellent");
+        } else if (s.equals("B")) {
+            bw.write("Good");
+        } else if (s.equals("C")) {
+            bw.write("Usually");
+        } else if (s.equals("D")) {
+            bw.write("Effort");
+        } else {
+            bw.write("Failure");
+        }
+
+        bw.flush();
+    }
+}
