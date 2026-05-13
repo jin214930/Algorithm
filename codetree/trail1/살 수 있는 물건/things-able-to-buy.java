b51e849f85ecde94fa6c400701553e1a2a51629e
김진명 <rlawlsaud2149@gmail.com>
2026-05-13 18:28:54 +0900

    [Passed] 살 수 있는 물건 (94ms, 9MB)

---
+++ codetree/trail1/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n >= 3000) {
+            bw.write("book");
+        } else if (n >= 1000) {
+            bw.write("mask");
+        } else {
+            bw.write("no");
+        }
+
+        bw.flush();
+    }
+}
