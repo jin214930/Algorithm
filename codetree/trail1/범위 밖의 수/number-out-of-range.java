44f27c2f6cc2ebf01bce65b64b7de1a5918ecdad
김진명 <rlawlsaud2149@gmail.com>
2026-05-14 19:21:25 +0900

    [Passed] 범위 밖의 수 (90ms, 9MB)

---
+++ codetree/trail1/범위 밖의 수/number-out-of-range.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        
+        if (a < 10 || a > 20) {
+            bw.write("yes");
+        } else {
+            bw.write("no");
+        }
+        
+        bw.flush();
+    }
+}
