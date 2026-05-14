79b370cb5d90dcf3008f01dcb552803a764f7959
김진명 <rlawlsaud2149@gmail.com>
2026-05-14 19:26:29 +0900

    [Passed] 굉장한 수 (88ms, 9MB)

---
+++ codetree/trail1/굉장한 수/amazing-number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+
+        if (a % 2 == 1 && a % 3 == 0 || a % 2 == 0 && a % 5 == 0) {
+            bw.write("true");
+        } else {
+            bw.write("false");
+        }
+
+        bw.flush();
+    }
+}
