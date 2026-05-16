c6a6dc5cda9eac12b2d8e28118e08979d403f61c
김진명 <rlawlsaud2149@gmail.com>
2026-05-16 20:13:50 +0900

    [Passed] 중앙값 구하기 (92ms, 9MB)

---
+++ codetree/trail1/중앙값 구하기/find-the-median.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] a = new int[3];
+        for (int i = 0; i < 3; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+
+        bw.write(a[1] + "\n");
+
+        bw.flush();
+    }
+}
