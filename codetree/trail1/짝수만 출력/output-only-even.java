add08aeee803216f80f7016f391239d468e09736
김진명 <rlawlsaud2149@gmail.com>
2026-05-18 09:24:31 +0900

    [Passed] 짝수만 출력 (87ms, 9MB)

---
+++ codetree/trail1/짝수만 출력/output-only-even.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int i = a;
+        while (i <= b) {
+            if (i % 2 == 0) {
+                bw.write(i + " ");
+            }
+            i++;
+        }
+
+        bw.flush();
+    }
+}
