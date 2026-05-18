d9a437c3d83273b4bef072db76d23a17314a5a19
김진명 <rlawlsaud2149@gmail.com>
2026-05-18 09:19:30 +0900

    [Passed] 증가시키며 출력하기 2 (90ms, 9MB)

---
+++ codetree/trail1/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int idx = 10;
+        while(idx <= 26) {
+            bw.write(idx + " ");
+            idx++;
+        }
+
+        bw.flush();
+    }
+}
