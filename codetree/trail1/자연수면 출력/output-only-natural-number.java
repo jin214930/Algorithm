3c1bea757ad0054310f885e9c604bbe81ec0d999
김진명 <rlawlsaud2149@gmail.com>
2026-05-19 09:14:25 +0900

    [Passed] 자연수면 출력 (90ms, 10MB)

---
+++ codetree/trail1/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > 0) {
+            for (int i = 0; i < b; i++) {
+                bw.write(a + "");
+            }
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+    }
+}
