00cd2b7d7487f3d743fed1172ec7e94b7c5b7761
김진명 <rlawlsaud2149@gmail.com>
2026-05-20 09:29:18 +0900

    [Passed] 100부터 입력받는 수까지의 합 (111ms, 11MB)

---
+++ codetree/trail1/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i = n; i <= 100; i++) {
+            sum += i;
+        }
+
+        bw.write(sum + "");
+
+        bw.flush();
+    }
+}
