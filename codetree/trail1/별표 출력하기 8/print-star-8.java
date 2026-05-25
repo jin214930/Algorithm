14b2e5d5e83ec4d8500dd2b2a29e3ccb2a29e1e1
김진명 <rlawlsaud2149@gmail.com>
2026-05-25 20:47:23 +0900

    [Passed] 별표 출력하기 8 (96ms, 9MB)

---
+++ codetree/trail1/별표 출력하기 8/print-star-8.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                bw.write("*");
+            } else {
+                for (int j = 0; j <= i; j++) {
+                    bw.write("* ");
+                }
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+}
