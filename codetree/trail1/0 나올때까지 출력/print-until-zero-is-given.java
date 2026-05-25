4f66784715d69b5b7acbaf6702ce15ee6421a74e
김진명 <rlawlsaud2149@gmail.com>
2026-05-25 14:49:28 +0900

    [Passed] 0 나올때까지 출력 (93ms, 9MB)

---
+++ codetree/trail1/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0)
+                break;
+            bw.write(n + "\n");
+        }
+
+        bw.flush();
+    }
+}
