97fd9849500ebfd913f1a92db705420dab3fafc4
김진명 <rlawlsaud2149@gmail.com>
2026-05-28 14:00:55 +0900

    [Passed] 배열 채우고 출력하기 (92ms, 9MB)

---
+++ codetree/trail1/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] a = br.readLine().split(" ");
+        for (int i = 9; i >= 0; i--) {
+            bw.write(a[i]);
+        }
+
+        bw.flush();
+    }
+}
