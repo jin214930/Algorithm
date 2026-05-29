584be853aa274a69f57e0f2610f209a5fe92e2f5
김진명 <rlawlsaud2149@gmail.com>
2026-05-29 11:52:17 +0900

    [Passed] 특정 위치의 수 (92ms, 9MB)

---
+++ codetree/trail1/특정 위치의 수/numbers-in-specific-location.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] a = new int[10];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        bw.write(a[2] + a[4] + a[9] + "");
+
+        bw.flush();
+    }
+}
