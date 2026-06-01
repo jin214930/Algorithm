a27c3e6b27f47f5ac0372920057ea3d75c472607
김진명 <rlawlsaud2149@gmail.com>
2026-06-01 09:45:36 +0900

    [Passed] 전항의 두 배 (95ms, 9MB)

---
+++ codetree/trail1/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a =  Integer.parseInt(st.nextToken());
+        int b =  Integer.parseInt(st.nextToken());
+        
+        bw.write(a + " " + b + " ");
+        for (int i = 0; i < 8; i++) {
+            int tmp= b;
+            b = b + 2 * a;
+            a = tmp;
+            bw.write(b + " ");
+        }
+
+        bw.flush();
+    }
+}
