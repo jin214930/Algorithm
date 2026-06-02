0e73ba5925a0555feb836c6bc24b25e0dbb142e7
김진명 <rlawlsaud2149@gmail.com>
2026-06-02 09:31:00 +0900

    [Passed] 1-9 개수 세기 (97ms, 9MB)

---
+++ codetree/trail1/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] cnt = new int[10];
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            cnt[x]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            bw.write(cnt[i] + "\n");
+        }
+
+        bw.flush();
+    }
+}
