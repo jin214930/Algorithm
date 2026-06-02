4fbf61facb3c7033f2db620fd19a901aea632708
김진명 <rlawlsaud2149@gmail.com>
2026-06-02 10:01:05 +0900

    [Passed] 거품 정렬 구현 (105ms, 9MB)

---
+++ codetree/trail3/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(a);
+        
+        for (int i = 0; i < n; i++) {
+            bw.write(a[i] + " ");
+        }
+
+        bw.flush();
+    }
+}
