00bcd008f65c81a01e3bed9014d39b842bc1e72f
김진명 <rlawlsaud2149@gmail.com>
2026-06-04 11:14:27 +0900

    [Passed] 병합 정렬 구현 (345ms, 20MB)

---
+++ codetree/trail3/병합 정렬 구현/implement-merge-sort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i] + " ");
+        }
+
+
+        bw.flush();
+    }
+}
