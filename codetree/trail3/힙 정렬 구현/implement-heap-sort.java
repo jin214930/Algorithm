4829e576baa16208a790f914a3d4ebcae6f68717
김진명 <rlawlsaud2149@gmail.com>
2026-06-06 10:05:49 +0900

    [Passed] 힙 정렬 구현 (324ms, 20MB)

---
+++ codetree/trail3/힙 정렬 구현/implement-heap-sort.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i] + " ");
+        }
+
+        bw.flush();
+    }
+}
