72fbfd7861b655e2fb93a48616782857b7fe050c
WARD <rlawlsaud2149@gmail.com>
2026-06-14 19:59:40 +0900

    [Passed] 특정 문자의 유무 (102ms, 9MB)

---
+++ codetree/trail1/특정 문자의 유무/specific-character-presence.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String s = br.readLine();
+        bw.write(s.contains("ee") ? "Yes " : "No ");
+        bw.write(s.contains("ab") ? "Yes" : "No");
+
+        bw.flush();
+    }
+}
