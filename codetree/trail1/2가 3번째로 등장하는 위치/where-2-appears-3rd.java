879545011c7dd63d5dc0d5a3442296f348ab6204
WARD <rlawlsaud2149@gmail.com>
2026-06-14 20:16:40 +0900

    [Passed] 2가 3번째로 등장하는 위치 (94ms, 9MB)

---
+++ codetree/trail1/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if (x == 2) cnt++;
+            if (cnt == 3) {
+                bw.write(i + 1 + "");
+                break;
+            }
+        }
+
+        bw.flush();
+    }
+}
