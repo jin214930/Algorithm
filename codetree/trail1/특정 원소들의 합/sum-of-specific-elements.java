aac1f435aa9377b2b95b138ae441902ef02a0788
WARD <rlawlsaud2149@gmail.com>
2026-06-15 21:18:56 +0900

    [Passed] 특정 원소들의 합 (91ms, 9MB)

---
+++ codetree/trail1/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                int x = Integer.parseInt(st.nextToken());
+                if (i >= j) sum += x;
+            }
+        }
+
+        bw.write(sum + "");
+
+
+        bw.flush();
+    }
+}
