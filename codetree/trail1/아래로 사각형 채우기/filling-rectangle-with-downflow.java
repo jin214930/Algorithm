5a2ebb3b436c18010a7b7d091a4f47e9f3d610e5
WARD <rlawlsaud2149@gmail.com>
2026-06-15 21:32:17 +0900

    [Passed] 아래로 사각형 채우기 (96ms, 9MB)

---
+++ codetree/trail1/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] a = new int[n][n];
+        int tmp = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[j][i] = tmp++;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                bw.write(a[i][j] + " ");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+}
