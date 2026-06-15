6d47a91a83b777e2d6cc27d616f4039150df3c44
WARD <rlawlsaud2149@gmail.com>
2026-06-15 21:10:30 +0900

    [Passed] 대문자로 바꾸기 (94ms, 9MB)

---
+++ codetree/trail1/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 0; i < 5; i++) {
+            bw.write(br.readLine().toUpperCase() + "\n");
+        }
+
+        bw.flush();
+    }
+}
