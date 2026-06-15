0799f0f93f1f1d4493f3deb6475cab701ee9c1c7
WARD <rlawlsaud2149@gmail.com>
2026-06-15 21:28:01 +0900

    [Passed] 두 배열의 곱 (90ms, 9MB)

---
+++ codetree/trail1/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[][] a = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        br.readLine();
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                a[i][j] *= Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                bw.write(a[i][j] + " ");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+}
