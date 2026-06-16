8e184909334c02354de8e07a24496ad2c47f06d5
WARD <rlawlsaud2149@gmail.com>
2026-06-16 09:45:26 +0900

    [Passed] 문자열 밀기 (92ms, 9MB)

---
+++ codetree/trail1/문자열 밀기/push-char.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        bw.write(s.substring(1));
+        bw.write(s.charAt(0));
+
+        bw.flush();
+    }
+}
