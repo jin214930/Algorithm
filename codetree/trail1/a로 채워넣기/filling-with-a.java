e67296092aa86951dc2179124e8bbe290820e3e7
WARD <rlawlsaud2149@gmail.com>
2026-06-16 09:22:10 +0900

    [Passed] a로 채워넣기 (96ms, 9MB)

---
+++ codetree/trail1/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();;
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 1 || i == s.length() - 2) {
+                bw.write('a');
+            } else {
+                bw.write(s.charAt(i));
+            }
+        }
+
+        bw.flush();
+    }
+}
