57ef64b1f434021de66007cd1ee62a40b50942c1
WARD <rlawlsaud2149@gmail.com>
2026-06-17 09:46:38 +0900

    [Passed] 정수만 더하기 (90ms, 9MB)

---
+++ codetree/trail1/정수만 더하기/add-only-integers.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        int sum = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sum += c - '0';
+            }
+        }
+
+        bw.write(sum + "");
+
+        bw.flush();
+    }
+}
