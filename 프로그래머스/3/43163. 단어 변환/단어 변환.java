6410d3fa11d9647786eedba794132e370fdbc93b
WARD <rlawlsaud2149@gmail.com>
2026-06-27 13:37:36 +0900

    [level 3] Title: 단어 변환, Time: 0.60 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,37 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public boolean check(String s1, String s2) {
-        int cnt = 0;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i))
-                cnt++;
-        }
-        
-        return cnt == 1;
-    }
-    
     public int solution(String begin, String target, String[] words) {
-        Map<String, Integer> map = new HashMap<>();
         Queue<String> q = new LinkedList<>();
-        
+        Map<String, Integer> visited = new HashMap<>();
         q.add(begin);
-        map.put(begin, 1);
+        visited.put(begin, 1);
         
         while(!q.isEmpty()) {
-            String s = q.poll();
-            if (s.equals(target)) break;
-            for(String word : words) {
-                if (map.containsKey(word)) continue;
-                if (!check(s, word)) continue;
-                q.add(word);
-                map.put(word, map.get(s) + 1);
+            String cur = q.poll();
+            if (cur.equals(target)) break;
+            for (String s : words) {
+                if (visited.containsKey(s) || !canChange(cur, s)) continue;
+                q.add(s);
+                visited.put(s, visited.get(cur) + 1);
             }
         }
         
-        if (map.containsKey(target))
-            return map.get(target) - 1;
+        if (!visited.containsKey(target)) return 0;
+        return visited.get(target) - 1;
         
-        return 0;
+    }
+    
+    boolean canChange(String a, String b) {
+        int cnt = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) cnt++;
+        }
+        
+        return cnt == 1;
     }
 }
