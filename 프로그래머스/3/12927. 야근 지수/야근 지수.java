418f1994989f2b6f0547518c93658efdf10affc4
WARD <rlawlsaud2149@gmail.com>
2026-06-27 10:59:55 +0900

    [level 3] Title: 야근 지수, Time: 66.09 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,22 +2,12 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        long sum = 0;
-        for (int work : works) {
-            sum += work;
-        }
-        
-        if (sum <= n) {
-            return 0;
-        }
-        
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int work : works) {
-            pq.add(work);
-        }
+        for (int work : works) pq.add(work);
         
         for (int i = 0; i < n; i++) {
             int x = pq.poll();
+            if (x == 0) return 0;
             pq.add(x - 1);
         }
         
