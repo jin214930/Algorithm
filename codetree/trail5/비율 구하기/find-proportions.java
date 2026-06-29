bdf73c6361953a3590e10afe110fc672c46ca369
WARD <rlawlsaud2149@gmail.com>
2026-06-29 14:11:23 +0900

    [Passed] 비율 구하기 (400ms, 14MB)

---
+++ codetree/trail5/비율 구하기/find-proportions.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            tm.put(s, tm.getOrDefault(s, 0) + 1);
+        }
+        
+        for (Map.Entry<String, Integer> entry : tm.entrySet()) {
+            bw.write(String.format("%s %.4f\n", entry.getKey(), entry.getValue() * 100. / n));
+        }
+
+        bw.flush();
+    }
+}
