7853deeb0b3079b46a5b5757ca1119ae6c9d0e55
WARD <rlawlsaud2149@gmail.com>
2026-06-30 10:33:15 +0900

    [Passed] top K 숫자 (432ms, 23MB)

---
+++ codetree/trail5/top K 숫자/top-k-elements.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        TreeSet<Integer> set = new TreeSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 0; i < k; i++) {
+            bw.write(set.pollLast() + " ");
+        }
+
+        bw.flush();
+    }
+}
