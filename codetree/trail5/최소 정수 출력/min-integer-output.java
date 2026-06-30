50bef86b5c119f736d76d0fb313959c52a5cb2ad
WARD <rlawlsaud2149@gmail.com>
2026-06-30 10:54:37 +0900

    [Passed] 최소 정수 출력 (419ms, 24MB)

---
+++ codetree/trail5/최소 정수 출력/min-integer-output.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.isEmpty()) bw.write(x + "\n");
+                else bw.write(pq.poll() + "\n");
+            } else pq.add(x);
+        }
+
+        bw.flush();
+    }
+}
