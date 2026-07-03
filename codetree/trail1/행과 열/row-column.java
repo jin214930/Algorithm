04a6b027c8ea92dd11f516381e26e7cadc21db75
WARD <rlawlsaud2149@gmail.com>
2026-07-03 20:19:17 +0900

    [Passed] 행과 열 (93ms, 11MB)

---
+++ codetree/trail1/행과 열/row-column.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                bw.write(i * j + " ");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+}
