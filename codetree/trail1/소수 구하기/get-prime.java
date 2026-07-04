fb14c34bdbb890ab21d58ee08870ebc8988cca66
WARD <rlawlsaud2149@gmail.com>
2026-07-04 11:36:46 +0900

    [Passed] 소수 구하기 (93ms, 9MB)

---
+++ codetree/trail1/소수 구하기/get-prime.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 2; i <= n; i++) {
+            boolean flag = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) bw.write(i + " ");
+        }
+
+        bw.flush();
+    }
+}
