8db2bd9191a19bb212e1b0b13d5afcea0d84d8a8
WARD <rlawlsaud2149@gmail.com>
2026-07-04 11:14:41 +0900

    [Passed] 알파벳 출력하기 (90ms, 9MB)

---
+++ codetree/trail1/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        char tmp = 'A';
+        for (int i = 0; i < n;i++) {
+            for (int j = 0; j <= i; j++) {
+                bw.write(tmp);
+                tmp = (char) ((tmp - 'A' + 1) % 26 + 'A');
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+}
