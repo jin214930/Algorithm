4f9e52d9048217c9e7c3d836b3e5865b618696ce
WARD <rlawlsaud2149@gmail.com>
2026-07-04 19:01:13 +0900

    [Passed] 체크 다이아몬드 (91ms, 9MB)

---
+++ codetree/trail1/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int tmp = 0;
+            for (int j = 0; j < n + i; j++) {
+                if (j < n - i - 1) bw.write(" ");
+                else {
+                    if (tmp % 2 == 0) bw.write("*");
+                    else bw.write(" ");
+                    tmp++;
+                }
+            }
+            bw.write("\n");
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int tmp = 0;
+            for (int j = 0; j < 2 * n - 2 - i; j++) {
+                if (j < i + 1) bw.write(" ");
+                else {
+                    if (tmp % 2 == 0) bw.write("*");
+                    else bw.write(" ");
+                    tmp++;
+                }
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
