eb580e14e65f36039253526f4bbcee7a6d9826a0
WARD <rlawlsaud2149@gmail.com>
2026-07-04 18:28:36 +0900

    [Passed] 특정 규칙에 따른 문자 출력 (96ms, 9MB)

---
+++ codetree/trail1/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < n - 1 - i) bw.write("  ");
+                else bw.write("@ ");
+            }
+            bw.write("\n");
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++)
+                bw.write("@ ");
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
