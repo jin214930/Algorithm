b7e87a556ab92b479a1e8385bfec97579bd7282d
WARD <rlawlsaud2149@gmail.com>
2026-07-05 10:43:53 +0900

    [Passed] 특정 구간의 합 (102ms, 9MB)

---
+++ codetree/trail2/특정 구간의 합/sum-of-specific-intervals.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] a = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int e = Integer.parseInt(st.nextToken()) - 1;
+            int ans = 0;
+            for (int j = s; j <= e; j++) {
+                ans += a[j];
+            }
+            bw.write(ans + "\n");
+        }
+
+        bw.flush();
+    }
+}
