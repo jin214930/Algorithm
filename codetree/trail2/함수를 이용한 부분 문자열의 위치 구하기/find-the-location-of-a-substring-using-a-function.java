8f21227225085237d21fd6cdcc05af2cde8a6581
WARD <rlawlsaud2149@gmail.com>
2026-07-05 09:35:50 +0900

    [Passed] 함수를 이용한 부분 문자열의 위치 구하기 (90ms, 9MB)

---
+++ codetree/trail2/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        String p = br.readLine();
+
+        bw.write(s.indexOf(p) + "");
+
+        bw.flush();
+    }
+}
