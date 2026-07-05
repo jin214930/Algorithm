148871449e5a4f2f4e79ff35b063f5892bbb1f93
WARD <rlawlsaud2149@gmail.com>
2026-07-05 10:36:49 +0900

    [Passed] 나누고 빼면서 합하기 (92ms, 9MB)

---
+++ codetree/trail2/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int[] a = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int ans = 0;
+        while(m > 0) {
+            ans += a[m - 1];
+            if (m % 2 == 0) m /= 2;
+            else m--;
+        }
+
+        bw.write(ans + "");
+        bw.flush();
+    }
+}
