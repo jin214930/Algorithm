77fed76cdf737c43ed30a0840116b44575a11bbf
WARD <rlawlsaud2149@gmail.com>
2026-07-05 16:43:03 +0900

    [Passed] 1이 되는 순간까지 (98ms, 9MB)

---
+++ codetree/trail2/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (n != 1) {
+            if (n % 2 == 0) n /= 2;
+            else n /= 3;
+            cnt++;
+        }
+
+        bw.write(cnt + "");
+
+        bw.flush();
+    }
+}
