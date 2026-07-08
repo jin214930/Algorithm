a935c1d8a14e06675807b8d5b7d574e6949b435b
WARD <rlawlsaud2149@gmail.com>
2026-07-08 14:35:51 +0900

    [Passed] 최대로 겹치는 지점 (93ms, 9MB)

---
+++ codetree/trail2/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] cnt = new int[101];
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for (int i = a; i <= b; i++) cnt[i]++;
+        }
+
+        int ans = 0;
+        for (int i = 0; i <= 100; i++) ans = Math.max(ans, cnt[i]);
+        bw.write(ans + "");
+
+        bw.flush();
+    }
+}
