39c828f38d01091e438d9d4d53b46e1b1c677ccb
WARD <rlawlsaud2149@gmail.com>
2026-07-08 13:55:01 +0900

    [Passed] 십진수로 변환하기 (101ms, 9MB)

---
+++ codetree/trail2/십진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        int x = Integer.parseInt(s, 2);
+        bw.write(x + "");
+
+
+        bw.flush();
+    }
+}
