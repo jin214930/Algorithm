f3ce6d2d00a43afbd9c6e2c12f217d5bdacfa64b
WARD <rlawlsaud2149@gmail.com>
2026-07-08 13:57:23 +0900

    [Passed] 십진수와 이진수 2 (94ms, 9MB)

---
+++ codetree/trail2/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String s = br.readLine();
+        int x = Integer.parseInt(s, 2) * 17;
+        bw.write(Integer.toBinaryString(x));
+
+        bw.flush();
+    }
+}
