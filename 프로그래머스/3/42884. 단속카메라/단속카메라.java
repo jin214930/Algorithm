e661596b1daf260bfdb325285a2639a66b096631
WARD <rlawlsaud2149@gmail.com>
2026-07-13 18:04:43 +0900

    [level 3] Title: 단속카메라, Time: 5.35 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,22 +1,19 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] routes) {
         Arrays.sort(routes, (r1, r2) -> {
-            if (r1[1] == r2[1])
-                return r1[0] - r2[0];
+            if (r1[1] == r2[1]) return r1[0] - r2[0];
             return r1[1] - r2[1];
         });
         
         int ans = 0;
-        int tmp = -30000;
+        int tmp = -30001;
         for (int[] route : routes) {
             if (tmp < route[0]) {
-                tmp = route[1];
                 ans++;
+                tmp = route[1];
             }
         }
-        
         return ans;
     }
 }
