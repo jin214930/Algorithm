4a0ca1dee68698d83d459c6cd483fad5b148621f
WARD <rlawlsaud2149@gmail.com>
2026-07-13 18:10:53 +0900

    [level 3] Title: 최고의 집합, Time: 0.22 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[] solution(int n, int s) {
-        if (n > s)
-            return new int[]{-1};
-        
+        if (s < n) return new int[]{-1};
         int[] ans = new int[n];
+        
         int tmp = s % n;
         for (int i = 0; i < n; i++) {
-            ans[i] = s / n;
-            if (i >= n - tmp) {
-                ans[i]++;
-            }
+            if (i < n - tmp) ans[i] = s / n;
+            else ans[i] = s / n + 1;
         }
-        
         return ans;
     }
 }
