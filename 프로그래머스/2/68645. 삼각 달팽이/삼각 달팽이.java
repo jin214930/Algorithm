fbf2f813255881b3a4cf53dcbaa9b4051381db12
WARD <rlawlsaud2149@gmail.com>
2026-07-14 15:54:22 +0900

    [level 2] Title: 삼각 달팽이, Time: 13.10 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,46 +1,31 @@
-/*
-1. 방향 벡터 정의
-2. 2차원 배열 정의
-3. 채우기
-4. 1차원 배열로 리턴
-*/
-
 class Solution {
     int[] dy = {1, 0, -1};
     int[] dx = {0, 1, -1};
-    
     public int[] solution(int n) {
-        int[][] map = new int[n][];
-        for(int i = 0; i < n; i++) {
-            map[i] = new int[i + 1];
-        }
-        
-        int y = 0, x = 0, d = 0, tmp = 1;
-        int cnt = n * (n + 1) / 2;
-        while(cnt-- > 0) {
-            map[y][x] = tmp++;
-            
+        int k = n * (n + 1) / 2;
+        int[][] a = new int[n][n];
+        int y = 0, x = 0, d = 0;
+        for (int i = 1; i <= k; i++) {
+            a[y][x] = i;
             int ny = y + dy[d];
             int nx = x + dx[d];
-            if (ny >= n || nx > ny || map[ny][nx] != 0) {
+            if (ny >= n || nx >= n || a[ny][nx] != 0) {
                 d = (d + 1) % 3;
                 ny = y + dy[d];
                 nx = x + dx[d];
             }
-            
             y = ny;
             x = nx;
         }
         
-        int[] ans = new int[n * (n + 1) / 2];
-        
+        int[] ans = new int[k];
         int idx = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j <= i; j++) {
-                ans[idx++] = map[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] == 0) break;
+                ans[idx++] = a[i][j]; 
             }
         }
-        
-        return ans;        
+        return ans;
     }
 }
