d2164ea333475de174316e7dff259bfd853f1e2c
WARD <rlawlsaud2149@gmail.com>
2026-07-14 13:30:13 +0900

    [level 3] Title: 여행경로, Time: 0.56 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,38 +1,35 @@
 import java.util.*;
 
 class Solution {
-    boolean[] used;
-    List<String> list = new ArrayList<>();
-    
-    void dfs(String[][] tickets, String tmp, int depth) {
-        if (depth == tickets.length) {
-            list.add(tmp);
-            return;
-        }
-        
-        for(int i = 0; i < tickets.length; i++) {
-            if (used[i]) continue;
-            if (tmp.endsWith(tickets[i][0])) {
-                used[i] = true;
-                dfs(tickets, tmp + tickets[i][1], depth + 1);
-                used[i] = false;
-            }
-        }
-    }
+    boolean[] visited;
+    int n;
+    String[] ans;
     
     public String[] solution(String[][] tickets) {
-        used = new boolean[tickets.length];
-
-        dfs(tickets, "ICN", 0);
+        Arrays.sort(tickets, (t1, t2) -> {
+            return t1[1].compareTo(t2[1]);
+        });
         
-        Collections.sort(list);
-        
-        String[] ans = new String[tickets.length + 1];
-        for(int i = 0; i < ans.length; i++) {
-            String s = list.get(0);
-            ans[i] = s.substring(3*i, 3*i + 3);
-        }
+        n = tickets.length;
+        ans = new String[n + 1];
+        visited = new boolean[n];
+        ans[0] = "ICN";
+        go("ICN", 0, tickets);
         
         return ans;
+    }
+    
+    boolean go(String cur, int depth, String[][] tickets) {
+        if (depth == n) return true;
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            if (tickets[i][0].equals(cur)) {
+                visited[i] = true;
+                ans[depth + 1] = tickets[i][1];
+                if (go(tickets[i][1], depth + 1, tickets)) return true;
+                visited[i] = false;
+            }
+        }
+        return false;
     }
 }
