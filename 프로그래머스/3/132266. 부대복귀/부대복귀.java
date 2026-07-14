3131e094ead41644a10e274ded3c3d2ba1c54b67
WARD <rlawlsaud2149@gmail.com>
2026-07-14 13:51:04 +0900

    [level 3] Title: 부대복귀, Time: 90.27 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -9,24 +9,23 @@ class Solution {
             adj.get(road[1]).add(road[0]);
         }
         
+        Queue<Integer> q = new LinkedList<>();
+        int[] visited = new int[n + 1];
+        q.add(destination);
+        visited[destination] = 1;
+        
+        while(!q.isEmpty()) {
+            int x = q.poll();
+            for (int nx : adj.get(x)) {
+                if (visited[nx] != 0) continue;
+                q.add(nx);
+                visited[nx] = visited[x] + 1;
+            }
+        }
+        
         int[] ans = new int[sources.length];
         for (int i = 0; i < sources.length; i++) {
-            int source = sources[i];
-            Queue<Integer> q = new LinkedList<>();
-            int[] visited = new int[n + 1];
-            q.add(source);
-            visited[source] = 1;
-            
-            while(!q.isEmpty()) {
-                int x = q.poll();
-                if (x == destination) break;
-                for (int nx : adj.get(x)) {
-                    if (visited[nx] != 0) continue;
-                    q.add(nx);
-                    visited[nx] = visited[x] + 1;
-                }
-            }
-            ans[i] = visited[destination] - 1;
+            ans[i] = visited[sources[i]] - 1;
         }
         
         return ans;
