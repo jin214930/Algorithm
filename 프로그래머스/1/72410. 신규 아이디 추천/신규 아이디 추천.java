b87c8e42cb0e01230101196b37574595df80ae2f
WARD <rlawlsaud2149@gmail.com>
2026-07-15 17:45:17 +0900

    [level 1] Title: 신규 아이디 추천, Time: 8.83 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,43 +1,42 @@
 class Solution {
     public String solution(String new_id) {
         String id = new_id.toLowerCase();
-        
         StringBuilder sb = new StringBuilder();
         for (char c : id.toCharArray()) {
-            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.')
+            if (c >= 'a' && c <= 'z' || c >= '0' && c <= '9' || c == '-' || c == '_' || c == '.')
                 sb.append(c);
         }
         
         id = sb.toString();
-        sb = new StringBuilder();
-        char tmp = '\0';
+        sb.setLength(0);
+        
+        boolean flag = false;
         for (char c : id.toCharArray()) {
-            if (c == '.' && tmp == '.')
-                continue;
-            sb.append(c);
-            tmp = c;
+            if (c == '.') {
+                if (flag) continue;
+                else {
+                    sb.append(c);
+                    flag = true;
+                }
+            } else {
+                sb.append(c);
+                flag = false;
+            }
         }
         
+        if (sb.length() > 0 && sb.charAt(0) == '.') sb.deleteCharAt(0);
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') sb.deleteCharAt(sb.length() - 1);
         id = sb.toString();
-        if (id.startsWith("."))
-            id = id.substring(1);
-        if (id.endsWith("."))
-            id = id.substring(0, id.length() - 1);
-        
-        if (id.length() == 0)
-            id = "a";
-        
+        if (id.length() == 0) id = "a";
         if (id.length() >= 16) {
             id = id.substring(0, 15);
-            if (id.endsWith("."))
-                id = id.substring(0, 14);
+            if (id.charAt(14) == '.') id = id.substring(0, 14);
         }
         
-        while (id.length() < 3)
+        while (id.length() < 3) {
             id = id + id.charAt(id.length() - 1);
+        }
         
         return id;
-        
-        
     }
 }
