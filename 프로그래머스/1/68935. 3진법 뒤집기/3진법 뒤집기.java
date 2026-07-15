6c6ebd8ba34e707f2066ac7e163180a2b8adace8
WARD <rlawlsaud2149@gmail.com>
2026-07-15 16:53:21 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,20 +1,8 @@
-class Solution {    
+class Solution {
     public int solution(int n) {
-        StringBuilder sb = new StringBuilder();
-        
-        while(n != 0) {
-            sb.append(n % 3);
-            n /= 3;
-        }
-        
-        String s = sb.toString();
-        int tmp = 1;
-        int ans = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            ans += tmp * (s.charAt(i) - '0');
-            tmp *= 3;
-        }
-        
-        return ans;
+        String s = Integer.toString(n, 3);
+        StringBuilder sb = new StringBuilder(s);
+        String r = sb.reverse().toString();
+        return Integer.parseInt(r, 3);
     }
 }
