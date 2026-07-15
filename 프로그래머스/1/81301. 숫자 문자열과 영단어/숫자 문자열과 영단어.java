60042120d3fed5b66590be6acea8ebf6d104ef64
WARD <rlawlsaud2149@gmail.com>
2026-07-15 17:03:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.79 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,52 +1,10 @@
 class Solution {
-    
     public int solution(String s) {
-        int ans = 0;
-        
-        int idx = 0;
-        while(idx < s.length()) {
-            char c = s.charAt(idx);
-            ans *= 10;
-            if(c >= '0' && c <= '9')
-                ans += c - '0';
-            else if (c == 'z') {
-                idx+=3;
-            } else if (c == 'o') {
-                idx += 2;
-                ans += 1;
-            } else if (c == 't') {
-                if (s.charAt(idx + 1) == 'w') {
-                    idx += 2;
-                    ans += 2;
-                } else {
-                    idx += 4;
-                    ans += 3;
-                }
-            } else if (c == 'f') {
-                if (s.charAt(idx + 1) == 'o') 
-                    ans += 4;
-                else
-                    ans += 5;
-                idx += 3;
-            } else if (c == 's') {
-                if (s.charAt(idx + 1) == 'i') {
-                    idx += 2;
-                    ans += 6;
-                } else {
-                    idx += 4;
-                    ans += 7;
-                }
-            } else if (c == 'e') {
-                idx += 4;
-                ans += 8;
-            } else {
-                idx += 3;
-                ans += 9;
-            }
-            
-            idx++;
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < 10; i++) {
+            s = s.replaceAll(arr[i], i + "");
         }
         
-        return ans;
+        return Integer.parseInt(s);
     }
 }
