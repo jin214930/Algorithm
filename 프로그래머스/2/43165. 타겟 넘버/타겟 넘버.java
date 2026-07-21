e880f3e744da4a7ecab95cb876eb1288f540221a
WARD <rlawlsaud2149@gmail.com>
2026-07-21 14:37:48 +0900

    [level 2] Title: 타겟 넘버, Time: 4.93 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,19 @@
 class Solution {
-    int ans = 0;
-    
-    void dfs(int[] numbers, int idx, int target, int sum) {
-        if (idx == numbers.length) {
-            if (target == sum)
-                ans++;
-            return;
-        }
-        
-        dfs(numbers, idx + 1, target, sum + numbers[idx]);
-        dfs(numbers, idx + 1, target, sum - numbers[idx]);
-        
-    }
+    static int ans = 0;
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, target, 0);
+        go(0, 0, numbers, target);
         
         return ans;
+    }
+    
+    static void go(int idx, int sum, int[] numbers, int target) {
+        if (idx == numbers.length) {
+            if (sum == target) ans++;
+            return;
+        } 
+        
+        go(idx + 1, sum - numbers[idx], numbers, target);
+        go(idx + 1, sum + numbers[idx], numbers, target);
     }
 }
