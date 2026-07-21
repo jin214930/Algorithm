6c9de043af8b301467b32f5edb862ef4076531b0
WARD <rlawlsaud2149@gmail.com>
2026-07-21 14:57:50 +0900

    [level 2] Title: 더 맵게, Time: 658.04 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,19 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for (int s : scoville)
-            pq.add(s);
+        for (int s : scoville) pq.add(s);
         
         int ans = 0;
-        while (true) {
-            if (pq.size() == 1 && pq.peek() < K)
-                return -1;
+        while (pq.peek() < K) {
+            if (pq.size() == 1) return -1;
             int s1 = pq.poll();
-            if (s1 >= K)
-                break;
             int s2 = pq.poll();
-            pq.add(s1 + s2 * 2);
+            pq.add(s1 + 2 * s2);
             ans++;
         }
         
