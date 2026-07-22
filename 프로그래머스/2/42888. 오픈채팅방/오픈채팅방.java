6b2fccd6595b8d8355885143052ba1a6eabff349
WARD <rlawlsaud2149@gmail.com>
2026-07-22 15:51:28 +0900

    [level 2] Title: 오픈채팅방, Time: 133.10 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,30 +3,35 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
         Map<String, String> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
         
         for (String s : record) {
             String[] tmp = s.split(" ");
             String cmd = tmp[0];
-            if (cmd.equals("Enter") || cmd.equals("Change")) {
-                map.put(tmp[1], tmp[2]);
-            }
-        }
-        
-        List<String> list = new ArrayList<>();
-        for (String s : record) {
-            String[] tmp = s.split(" ");
-            if (tmp[0].equals("Enter")) {
-                String nickname = map.get(tmp[1]);
-                list.add(nickname + "님이 들어왔습니다.");
-            } else if (tmp[0].equals("Leave")) {
-                String nickname = map.get(tmp[1]);
-                list.add(nickname + "님이 나갔습니다.");
+            String id = tmp[1];
+            String name;
+            if (cmd.equals("Enter")) {
+                name = tmp[2];
+                list.add(cmd + ":" + id);
+                map.put(id, name);
+            } else if (cmd.equals("Leave")) {
+                list.add(cmd + ":" + id);
+            } else {
+                name = tmp[2];
+                map.put(id, name);
             }
         }
         
         String[] ans = new String[list.size()];
         for (int i = 0; i < list.size(); i++) {
-            ans[i] = list.get(i);
+            String s = list.get(i);
+            String[] tmp = s.split(":");
+            String cmd = tmp[0];
+            if (cmd.equals("Enter")) {
+                ans[i] = map.get(tmp[1]) + "님이 들어왔습니다.";
+            } else {
+                ans[i] = map.get(tmp[1]) + "님이 나갔습니다.";
+            }
         }
         
         return ans;
