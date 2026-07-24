9e725b8422db93d93281bf40ea64bfa517439217
WARD <rlawlsaud2149@gmail.com>
2026-07-24 20:11:42 +0900

    [level 2] Title: 방문 길이, Time: 2.74 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,62 +1,53 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String dirs) {
-        int y = 0;
-        int x = 0;
+    class Path {
+        int y1, x1, y2, x2;
+        Path(int y1, int x1, int y2, int x2) {
+            this.y1 = y1;
+            this.x1 = x1;
+            this.y2 = y2;
+            this.x2 = x2;
+        }
         
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Path path)) return false;
+            
+            return (y1 == path.y1 && x1 == path.x1 && y2 == path.y2 && x2 == path.x2)
+                || (y1 == path.y2 && x1 == path.x2 && y2 == path.y1 && x2 == path.x1);
+        }
+        
+        @Override
+        public int hashCode() {
+            int hash1 = Objects.hash(y1, x1);
+            int hash2 = Objects.hash(y2, x2);
+            return hash1 + hash2;
+        }
+    }
+    
+    public int solution(String dirs) {
+        int y = 0, x = 0;
         Set<Path> set = new HashSet<>();
         for (char c : dirs.toCharArray()) {
-            int ny = y;
-            int nx = x;
+            int ny = y, nx = x;
             if (c == 'U') {
-                ny += 1;
+                ny = y + 1;
             } else if (c == 'D') {
-                ny -= 1;
-            } else if (c == 'L') {
-                nx -= 1;
+                ny = y - 1;
             } else if (c == 'R') {
-                nx += 1;
+                nx = x + 1;
+            } else {
+                nx = x - 1;
             }
             
             if (ny < -5 || ny > 5 || nx < -5 || nx > 5) continue;
-            
             set.add(new Path(y, x, ny, nx));
             y = ny;
             x = nx;
         }
         
         return set.size();
-    }
-    
-    class Path {
-        int sy, sx, ey, ex;
-        
-        public Path(int y1, int x1, int y2, int x2) {
-            if (y1 < y2 || (y1 == y2 && x1 < x2)) {
-                sy = y1;
-                sx = x1;
-                ey = y2;
-                ex = x2;
-            } else {
-                sy = y2;
-                sx = x2;
-                ey = y1;
-                ex = x1;
-            }
-        }
-        
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Path path = (Path) o;
-            return sy == path.sy && sx == path.sx && ey == path.ey && ex == path.ex;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(sy, sx, ey, ex);
-        }
     }
 }
