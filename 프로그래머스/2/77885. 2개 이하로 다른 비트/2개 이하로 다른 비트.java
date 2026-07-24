64794ee9cd279c1061d89e3e7f6ffed5f65e66e5
WARD <rlawlsaud2149@gmail.com>
2026-07-24 20:34:53 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 46.15 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,21 +1,18 @@
 class Solution {
     public long[] solution(long[] numbers) {
         long[] ans = new long[numbers.length];
-        
         for (int i = 0; i < numbers.length; i++) {
-            long x = numbers[i];
-            
-            if (x % 2 == 0) {
-                ans[i] = x + 1;
+            String s = Long.toBinaryString(numbers[i]);
+            if (s.charAt(s.length() - 1) == '0') {
+                ans[i] = Long.parseLong(s.substring(0, s.length() - 1) + "1", 2);
             } else {
-                long bit = 1;
-                
-                while ((x & bit) != 0) {
-                    bit <<= 1;
+                int idx = s.lastIndexOf('0');
+                if (idx == -1) {
+                    ans[i] = Long.parseLong("10" + s.substring(1), 2);
+                } else {
+                    ans[i] = Long.parseLong(s.substring(0, idx) + "10" + s.substring(idx + 2), 2);
                 }
-                
-                ans[i] = (x | bit) & ~(bit >> 1);
-            }
+            }            
         }
         
         return ans;
