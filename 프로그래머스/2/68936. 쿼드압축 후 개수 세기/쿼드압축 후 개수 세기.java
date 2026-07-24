2cbf9262b0ae41f3420e321144826c84a302717b
WARD <rlawlsaud2149@gmail.com>
2026-07-24 18:46:28 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 5.68 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,30 +1,24 @@
 class Solution {
     int[] ans = new int[2];
+    public int[] solution(int[][] arr) {
+        go(0, 0, arr.length, arr);
+        
+        return ans;
+    }
     
     void go(int y, int x, int n, int[][] arr) {
-        if (n == 1) {
-            ans[arr[y][x]]++;
-            return;
-        }
-        
         int tmp = arr[y][x];
-        for(int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (tmp != arr[y + i][x + j]) {
+        for (int i = y; i < y + n; i++) {
+            for (int j = x; j < x + n; j++) {
+                if (tmp != arr[i][j]) {
                     go(y, x, n / 2, arr);
-                    go(y + n / 2, x, n/2, arr);
-                    go(y, x + n/2, n/2, arr);
-                    go(y + n/2,x+n/2, n/2,arr);
+                    go(y + n / 2, x, n / 2, arr);
+                    go(y, x + n / 2, n / 2, arr);
+                    go(y + n / 2, x + n / 2, n / 2, arr);
                     return;
                 }
             }
         }
         ans[tmp]++;
-    }
-    
-    public int[] solution(int[][] arr) {
-        go(0, 0, arr.length, arr);
-        
-        return ans;
     }
 }
