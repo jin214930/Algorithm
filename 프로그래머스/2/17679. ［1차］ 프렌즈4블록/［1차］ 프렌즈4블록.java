5d71c5dd691440010c36da32c04fa8d4e1dc3e58
WARD <rlawlsaud2149@gmail.com>
2026-07-25 23:23:37 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 7.41 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,53 +1,60 @@
-import java.util.*;
-
 class Solution {
+    static char[][] a;
+    
     public int solution(int m, int n, String[] board) {
-        char[][] a = new char[m][n];
+        a = new char[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 a[i][j] = board[i].charAt(j);
             }
         }
         
-        while(true) {
-            boolean flag = true;
+        int ans = 0;
+        while (true) {            
+            boolean isBomb = false;
             boolean[][] check = new boolean[m][n];
             for (int i = 0; i < m - 1; i++) {
                 for (int j = 0; j < n - 1; j++) {
-                    if (a[i][j] != ' ' && a[i][j] == a[i][j + 1] && a[i][j] == a[i + 1][j] && a[i][j] == a[i + 1][ j + 1]) {
-                        flag = false;
-                        check[i][j] = check[i + 1][j] = check[i][j + 1] = check[i + 1][j + 1] = true;
+                    char c = a[i][j];
+                    if (c == '\0') continue;
+                    boolean isSame = true;
+                    for (int k = 0; k < 2; k++) {
+                        for (int l = 0; l < 2; l++) {
+                            if (c != a[i + k][j + l]) isSame = false;
+                        }
+                    }
+                    if (isSame) {
+                        for (int k = 0; k < 2; k++) {
+                            for (int l = 0; l < 2; l++) {
+                                check[i + k][j + l] = true;
+                            }
+                        }
+                        isBomb = true;
                     }
                 }
             }
             
-            if (flag) break;
+            if (!isBomb) break;
             
-            char[][] tmp = new char[m][n];
-            for (int i = 0; i < m; i++) 
-                Arrays.fill(tmp[i], ' ');
+            int cnt = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (check[i][j]) {
+                        a[i][j] = '\0';
+                        cnt++;
+                    }
+                }
+            }
+            ans += cnt;
             
+            char[][] newA = new char[m][n];
             for (int i = 0; i < n; i++) {
                 int idx = m - 1;
                 for (int j = m - 1; j >= 0; j--) {
-                    if (check[j][i] || a[j][i] == ' ') continue;
-                    tmp[idx][i] = a[j][i];
-                    idx--;
+                    if (a[j][i] != '\0') newA[idx--][i] = a[j][i];
                 }
             }
-            
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    a[i][j] = tmp[i][j];
-                }
-            }
-        }
-        
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (a[i][j] == ' ') ans++;
-            }
+            a = newA;
         }
         
         return ans;
