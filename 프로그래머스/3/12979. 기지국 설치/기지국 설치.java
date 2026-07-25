0d7b6504550e0b8ce45a4552def5172e662e7b94
WARD <rlawlsaud2149@gmail.com>
2026-07-25 23:00:18 +0900

    [level 3] Title: 기지국 설치, Time: 0.48 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,16 +1,21 @@
-class Solution {
+class Solution {    
     public int solution(int n, int[] stations, int w) {
         int ans = 0;
-        int tmp = 1;
-        for (int s : stations) {
-            if (tmp < s - w) {
-                ans += (s - w - tmp + 2 * w) / (2 * w + 1);
-            
-            } 
-            tmp = s + w + 1;
+        int x = 1;
+        for (int station : stations) {
+            if (x > n) break;
+            int s = station - w;
+            int e = station + w;
+            if (x < s) {
+                ans += (s - x + 2 * w) / (2 * w + 1);
+            }
+            x = e + 1;
         }
         
-        ans += (n + 1 - tmp + 2 * w) / (2 * w + 1);
+        if (x <= n) {
+            ans += (n + 1 - x + 2 * w) / (2 * w + 1);
+        }
+        
         return ans;
     }
 }
