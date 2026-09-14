a4b715c7644703d3def448acbd3b09f96024a6ec
WARD <rlawlsaud2149@gmail.com>
2026-09-14 10:00:40 +0900

    [level 3] Title: 부대복귀, Time: 112.44 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,25 +1,31 @@
 import java.util.*;
 
-class Solution {
+class Solution {    
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) adj.add(new ArrayList<>());
-        for (int[] road : roads) {
-            adj.get(road[0]).add(road[1]);
-            adj.get(road[1]).add(road[0]);
+        
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for (int[] r : roads) {
+            adj.get(r[0]).add(r[1]);
+            adj.get(r[1]).add(r[0]);
         }
         
         Queue<Integer> q = new LinkedList<>();
         int[] visited = new int[n + 1];
+        
         q.add(destination);
         visited[destination] = 1;
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int x = q.poll();
+            
             for (int nx : adj.get(x)) {
                 if (visited[nx] != 0) continue;
-                q.add(nx);
                 visited[nx] = visited[x] + 1;
+                q.add(nx);
             }
         }
         
